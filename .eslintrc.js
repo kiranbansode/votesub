c8ae6c0c162fa43ec18245c163749fd82e6df568
kiranbansode <kiran5120135@gmail.com>
2021-11-01 11:44:43 +0530

    chore: add and change config from eslint, prettier and vite config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,17 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { EsLinter, linterPlugin, TypeScriptLinter } from 'vite-plugin-linter';
+import { VitePWA as vitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    vitePWA(),
+    linterPlugin({
+      include: ['./src/**/*.ts', './src/**/*.tsx'],
+      linters: [new EsLinter({ configEnv }), new TypeScriptLinter()],
+    }),
+  ],
+
+  // Absolute imports (aliases)
+  resolve: {
+    alias: [
+      { find: 'assets', replacement: '/src/assets' },
+      { find: 'components', replacement: '/src/components' },
+      { find: 'config', replacement: '/src/config' },
+      { find: 'hooks', replacement: '/src/hooks' },
+      { find: 'layouts', replacement: '/src/layouts' },
+      { find: 'pages', replacement: '/src/pages' },
+      { find: 'store', replacement: '/src/store' },
+      { find: 'styled', replacement: '/src/styled' },
+      { find: 'utils', replacement: '/src/utils' },
+    ],
+  },
+}));

4694305c1f91bd38420eb83e8226f19eb5b12dbb
kiranbansode <kiran5120135@gmail.com>
2021-11-23 13:16:37 +0530

    feat(TextInputField): add noLabel and separateLabel props
    following props were added for more functionality
        - noLabel: inputLabel value will be used as placeholder text. No other label will be shown.
        - separateLabel : label will be detached from input field. No placeholder text.

--- src/components/TextInputField/index.tsx
+++ src/components/TextInputField/index.tsx
@@ -1,32 +1,62 @@
+/* eslint-disable no-nested-ternary */
 /* 
   Author : Kiran A. Bansode
 */
 
 import { UseFormRegisterReturn } from 'react-hook-form';
-import { TextField } from '@mui/material';
+import { TextField, FormControl, FormHelperText } from '@mui/material';
 import InputFieldWrapper from 'styled/InputFieldWrapper';
-import InputLabelWrapper from 'styled/InputLabelWrapper';
+import SeparateLabel from 'components/SeparateLabel';
 
 import './TextInputField.styles.scss';
 
 interface TextInputFieldPropsType {
   useFormRegister: UseFormRegisterReturn;
+  inputLabel: string;
+  inputHelperText?: string;
+  separateLabel?: boolean;
+  noLabel?: boolean;
 }
 
-const TextInputField = ({ useFormRegister }: TextInputFieldPropsType) => (
+const TextInputField = ({
+  separateLabel,
+  noLabel,
+  useFormRegister,
+  inputLabel,
+  inputHelperText,
+}: TextInputFieldPropsType) => (
   <>
     <InputFieldWrapper>
-      <InputLabelWrapper className="separate__label">Label</InputLabelWrapper>
-      <TextField
-        fullWidth
-        id={useFormRegister.name}
-        inputRef={useFormRegister.ref}
-        name={useFormRegister.name}
-        onBlur={useFormRegister.onBlur}
-        onChange={useFormRegister.onChange}
-      />
+      <FormControl>
+        {separateLabel ? (
+          <SeparateLabel
+            className="separate-label"
+            htmlFor={useFormRegister.name}
+            label={inputLabel}
+          />
+        ) : null}
+
+        <TextField
+          fullWidth
+          id={useFormRegister.name}
+          inputRef={useFormRegister.ref}
+          label={!noLabel ? (!separateLabel ? inputLabel : null) : null}
+          name={useFormRegister.name}
+          placeholder={noLabel ? inputLabel : ''}
+          onBlur={useFormRegister.onBlur}
+          onChange={useFormRegister.onChange}
+        />
+
+        <FormHelperText id={useFormRegister.name}>{inputHelperText}</FormHelperText>
+      </FormControl>
     </InputFieldWrapper>
   </>
 );
+
+TextInputField.defaultProps = {
+  separateLabel: false,
+  noLabel: false,
+  inputHelperText: '',
+};
 
 export default TextInputField;

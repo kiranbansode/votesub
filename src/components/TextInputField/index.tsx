0829271df457d46c0d54901a779036740e8039a3
kiranbansode <kiran5120135@gmail.com>
2022-02-03 14:49:59 +0530

    docs: add JSDoc comments to TextInputField

--- src/components/TextInputField/index.tsx
+++ src/components/TextInputField/index.tsx
@@ -19,20 +19,37 @@ interface TextInputFieldPropsType {
   autoFocus?: boolean;
 }
 
+/**
+ * Generate TextInput Field
+ * @author Kiran A. Bansode <kiran5120135@gmail.com>
+ * @param autoFocus - bring user attention to input field
+ * @param separateLabel - detach label from input field
+ * @param noLabel - label will not be shown
+ * @param inputHelperText - guidence or error message will be shown below input field
+ * @param inputLabel - label for input field
+ * @param inputPlaceHolder - placeholder inside input field
+ * @param useFormRegister - register() method from useForm() [React Hook Form]
+ * @return return TextInputField [JSX.Element]
+ */
+
 const TextInputField = ({
+  autoFocus,
   separateLabel,
   noLabel,
-  useFormRegister,
-  inputLabel,
   inputHelperText,
+  inputLabel,
   inputPlaceHolder,
-  autoFocus,
+  useFormRegister,
 }: TextInputFieldPropsType) => {
   let labelToShow;
+
   if (!noLabel) {
+    /* Depending on passed conditon it will do one of follwing
+    1. Separate Label - Detached from TextInputField
+    2. No Label - Label will not be showed
+    3. Default - Label will be shown in top border of TextInputField
+  */
     labelToShow = !separateLabel ? inputLabel : null;
-  } else {
-    labelToShow = null;
   }
 
   return (
@@ -68,11 +85,11 @@ const TextInputField = ({
 
 TextInputField.defaultProps = {
   autoFocus: false,
+  separateLabel: false,
+  noLabel: false,
   inputHelperText: '',
   inputLabel: 'TextField',
   inputPlaceHolder: '',
-  noLabel: false,
-  separateLabel: false,
 };
 
 export default TextInputField;

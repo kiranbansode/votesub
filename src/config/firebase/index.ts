85cdd3a0602cadf811aad14a6512cd2f6017aa53
kiranbansode <kiran5120135@gmail.com>
2022-02-03 13:42:59 +0530

    chore(firebase): export some firebase functionality as named export

--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -16,11 +16,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const firestore = getFirestore();
-const storage = getStorage();
-const functions = getFunctions(app);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const firestore = getFirestore();
+export const storage = getStorage();
+export const functions = getFunctions(app);
 
 // automatically connect to Firebase Emulators in localhost
 if (window.location.hostname === 'localhost') {

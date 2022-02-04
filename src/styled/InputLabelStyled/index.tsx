fd4544a3a166c717aace6a5042706a9d09f6efcf
kiranbansode <kiran5120135@gmail.com>
2022-02-04 13:50:11 +0530

    refactor: file rename

---
+++ src/styled/InputLabelStyled/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const InputLabelStyled = styled.label`
+  font: inherit;
+  letter-spacing: inherit;
+  font-size: 1.2rem;
+  color: #656973;
+  margin-bottom: 30px;
+`;
+
+export default InputLabelStyled;
--- src/styled/InputLabelWrapper/index.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const InputLabelWrapper = styled.p`
-  font-size: 20px;
-`;
-
-export default InputLabelWrapper;

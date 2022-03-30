2577a623c5c8fe0aa2f393c81d2f2f95a1843f02
kiranbansode <kiran5120135@gmail.com>
2022-03-30 20:49:45 +0530

    feat(pages): this completes UI work of LoginPage
    This commit completes the UI work of LoginPage as per Figma Design

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,20 +1,56 @@
 import { useForm } from 'react-hook-form';
+import useAppDisparch from 'hooks/useAppDispatch';
 
 import TextInputField from 'components/TextInputField';
 import PasswordInputField from 'components/PasswordInputField';
 import Button from 'components/Button';
+import Separator from 'components/Separator';
+import { userLogIn } from 'store/loginPage/userLoginSlice';
+
+import './LoginPage.styles.scss';
+import Logo from 'components/Logo';
+import Caption from 'components/Caption';
+
+export type userLoginFormTypes = {
+  username: string;
+  password: string;
+};
 
 const LoginPage = () => {
   const { register, handleSubmit } = useForm();
+  const dispatch = useAppDisparch();
 
   return (
-    <div id="login-page">
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
-        <TextInputField useFormRegister={register('username')} />
+    <div className="page" id="login-page">
+      <form onSubmit={handleSubmit((data: userLoginFormTypes) => dispatch(userLogIn(data)))}>
+        <Logo />
 
-        <PasswordInputField useFormRegister={register('password')} />
+        <Caption />
 
-        <Button>Login</Button>
+        <TextInputField
+          autoFocus
+          separateLabel
+          inputLabel="Username"
+          useFormRegister={register('username')}
+        />
+
+        <PasswordInputField
+          separateLabel
+          inputLabel="Password"
+          useFormRegister={register('password')}
+        />
+
+        <Button className="login-button" type="submit">
+          Login
+        </Button>
+
+        <Separator />
+
+        <p>
+          Don&#39;t have an account? Click on <span className="register-link">Register</span> button
+          below
+        </p>
+
         <Button color="success">Register</Button>
       </form>
     </div>

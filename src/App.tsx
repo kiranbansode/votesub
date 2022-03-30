ed557ca4367f8e437ad4c26481e82c54d0d926e2
kiranbansode <kiran5120135@gmail.com>
2022-03-30 21:01:41 +0530

    chore: add warning for user to use this website only on Mobile phones for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-// import LoginPage from 'pages/LoginPage';
-import RegistrationPage from 'pages/RegistrationPage';
+import LoginPage from 'pages/LoginPage';
+// import RegistrationPage from 'pages/RegistrationPage';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <LoginPage /> */}
+      <p style={{ textAlign: 'center', color: 'red', margin: '0' }}>Only For Mobiles</p>
+      <p style={{ textAlign: 'center', margin: '0', fontSize: '10px' }}> Try on a mobile device</p>
 
-      <RegistrationPage />
+      <LoginPage />
+
+      {/* <RegistrationPage /> */}
     </div>
   );
 }

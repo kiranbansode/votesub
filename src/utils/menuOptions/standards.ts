14e18f4973eea4c76376e257c5156307429c066f
Kiran Ashok Bansode <kiranbansode>
2022-05-06 13:32:54 +0530

    feat: add Standard and Division options for SelectInputField component

---
+++ src/utils/menuOptions/standards.ts
@@ -0,0 +1,17 @@
+export interface Option {
+  value: string | number;
+  option: string;
+}
+
+export const standardOptions: Option[] = [
+  { value: '1', option: 'I' },
+  { value: '2', option: 'II' },
+  { value: '3', option: 'III' },
+  { value: '4', option: 'IV' },
+  { value: '5', option: 'V' },
+  { value: '6', option: 'VI' },
+  { value: '7', option: 'VII' },
+  { value: '8', option: 'VIII' },
+  { value: '9', option: 'IX' },
+  { value: '10', option: 'X' },
+];

e3c8c358add608bf0b21eb96ae511fb5ed88cca1
kiranbansode <kiran5120135@gmail.com>
2022-05-28 15:18:07 +0530

    feat(userLoginSlice): add more properties in userLoginSlice

--- src/store/loginPage/userLoginSlice.ts
+++ src/store/loginPage/userLoginSlice.ts
@@ -1,71 +1,109 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import { UserLoginFormTypes } from 'pages/LoginPage';
-import { signInWithEmailAndPassword, UserInfo } from 'firebase/auth';
+import { signInWithEmailAndPassword, UserInfo, UserCredential } from 'firebase/auth';
 import { auth } from 'config/firebase';
+import authErrorMessageFinder from 'utils/helperFunctions/authErrorMessageFinder';
 
 export const userLogIn = createAsyncThunk(
-  'user/LogIn',
-  async (data: UserLoginFormTypes, { rejectWithValue }) => {
-    try {
-      const response = await signInWithEmailAndPassword(auth, data.username, data.password);
-      const {
-        user: { uid, email },
-      } = response;
+    'user/LogIn',
+    async (data: UserLoginFormTypes, { rejectWithValue }) => {
+        try {
+            const userCredentials: UserCredential = await signInWithEmailAndPassword(
+                auth,
+                data.username,
+                data.password,
+            );
+            const { user } = userCredentials;
 
-      return {
-        uid,
-        email,
-      };
-    } catch (error) {
-      return rejectWithValue(error);
-    }
-  },
+            return {
+                uid: user.uid,
+                email: user.email,
+                isEmailVerified: user.emailVerified,
+                username: user.displayName,
+                profilePhoto: user.photoURL,
+            };
+        } catch (error: any) {
+            const authErrorMessage = authErrorMessageFinder(error);
+            return rejectWithValue({
+                name: error.name,
+                code: error.code,
+                message: authErrorMessage,
+                customData: error.customData,
+            });
+        }
+    },
 );
 
-export type UserInfoType = {
-  userDetails: {
-    uid: UserInfo['uid'];
-    email: UserInfo['email'];
-  };
-  loading: boolean;
-};
+export interface IUserInfo {
+    userDetails: {
+        uid: UserInfo['uid'];
+        email: UserInfo['email'];
+        isEmailVerified: boolean | null;
+        username: UserInfo['displayName'];
+        profilePhoto: UserInfo['photoURL'];
+    };
+    loading: boolean;
+    error: any;
+}
 
-const initialState: UserInfoType = {
-  userDetails: {
-    email: '',
-    uid: '',
-  },
-  loading: false,
+const initialState: IUserInfo = {
+    userDetails: {
+        uid: '',
+        email: '',
+        isEmailVerified: null,
+        username: '',
+        profilePhoto: '',
+    },
+    loading: false,
+    error: {},
 };
 
 const userLoginSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(userLogIn.pending, (state) => {
-      state.userDetails.uid = '';
-      state.userDetails.email = '';
-      state.loading = true;
-    });
+    name: 'user',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(userLogIn.pending, (state) => {
+            state.loading = true;
+        });
 
-    /* 
-      !any should not be used
-      !change it to proper PayloadAction type
-      may find possible solution here : https://github.com/reduxjs/redux-toolkit/pull/827
-    */
-    builder.addCase(userLogIn.fulfilled, (state, action: PayloadAction<any>) => {
-      state.userDetails.uid = action.payload.uid;
-      state.userDetails.email = action.payload.email;
-      state.loading = false;
-    });
+        /*
+         * As per below link, there is a bug in RTK.
+         * https://github.com/reduxjs/redux-toolkit/issues/1707#issuecomment-962883993
+         * So, as for now use `any` for PayloadAction type
+         */
 
-    builder.addCase(userLogIn.rejected, (state) => {
-      state.userDetails.uid = '';
-      state.userDetails.email = null;
-      state.loading = false;
-    });
-  },
+        /* TODO: Remove any type and use proper one */
+        //! PayloadAction types should not be any
+        builder.addCase(userLogIn.fulfilled, (state, action: PayloadAction<any>) => {
+            state.userDetails = action.payload;
+            state.loading = false;
+        });
+
+        //! PayloadAction types should not be any
+        builder.addCase(userLogIn.rejected, (state, action: PayloadAction<any>) => {
+            state.error = action.payload;
+            state.loading = false;
+        });
+    },
 });
 
 export default userLoginSlice.reducer;
+
+// from PayloadAction<IUserInfo['userDetails']>
+//    payload: {
+//     uid: UserInfo['uid'];
+//     email: UserInfo['email'];
+//     isEmailVerified: boolean | null;
+//     username: UserInfo['displayName'];
+//     profilePhoto: UserInfo['photoURL'];
+// }
+
+// from IUserInfo['userDetails']
+//   userDetails: WritableDraft<{
+//     uid: UserInfo['uid'];
+//     email: UserInfo['email'];
+//     isEmailVerified: boolean | null;
+//     username: UserInfo['displayName'];
+//     profilePhoto: UserInfo['photoURL'];
+// }>

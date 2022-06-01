9bd85da13567f9b474767b7a60b056bc62fca18e
kiranbansode <kiran5120135@gmail.com>
2022-06-01 22:54:10 +0530

    feat(LoginPage): add Alert to LoginPage
    When use entered wrong password an Alert will be shown

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,81 +1,103 @@
 import { Link } from 'react-router-dom';
 import { useForm, FieldValues } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import useAppDisparch from 'hooks/useAppDispatch';
 import useAppSelector from 'hooks/useAppSelector';
 import TextInputField from 'components/TextInputField';
 import PasswordInputField from 'components/PasswordInputField';
 import Button from 'components/Button';
 import Separator from 'components/Separator';
-import { userLogIn } from 'store/loginPage/userLoginSlice';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-
-import './LoginPage.styles.scss';
 import Logo from 'components/Logo';
 import Caption from 'components/Caption';
+import { userLogIn } from 'store/loginPage/userLoginSlice';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import InputFieldWrapper from 'styled/InputFieldWrapper';
+
+import './LoginPage.styles.scss';
 
 export type UserLoginFormTypes = {
-  username: string;
-  password: string;
+    username: string;
+    password: string;
 };
 
 const loginPageFormDefaultValues: UserLoginFormTypes = {
-  username: '',
-  password: '',
+    username: '',
+    password: '',
 };
 
 const loginPageFormValidation = yup.object({
-  username: yup.string().required('Username is required'),
-  password: yup.string().required('Password is required'),
+    username: yup
+        .string()
+        .strict()
+        .trim('Blank Spaces are not allowed')
+        .email('Username must be a valid Email ID')
+        .max(255)
+        .required('Username is required'),
+    password: yup.string().strict().required('Password is required'),
 });
 
 const LoginPage = () => {
-  const { register, handleSubmit, formState } = useForm<FieldValues>({
-    defaultValues: loginPageFormDefaultValues,
-    resolver: yupResolver(loginPageFormValidation),
-  });
-  const dispatch = useAppDisparch();
-  const userState = useAppSelector(({ user }) => user);
+    const { register, handleSubmit, formState } = useForm<FieldValues>({
+        defaultValues: loginPageFormDefaultValues,
+        resolver: yupResolver(loginPageFormValidation),
+    });
+    const dispatch = useAppDisparch();
+    const userState = useAppSelector(({ user }) => user);
 
-  return (
-    <div className="page" id="login-page">
-      <form onSubmit={handleSubmit((data: UserLoginFormTypes) => dispatch(userLogIn(data)))}>
-        <Logo />
+    return (
+        <div className="page" id="login-page">
+            <form onSubmit={handleSubmit((data: UserLoginFormTypes) => dispatch(userLogIn(data)))}>
+                <Logo />
 
-        <Caption />
+                <Caption />
 
-        <TextInputField
-          autoFocus
-          separateLabel
-          errors={formState.errors}
-          inputLabel="Username"
-          useFormRegister={register('username')}
-        />
+                <TextInputField
+                    autoFocus
+                    separateLabel
+                    errors={formState.errors}
+                    formRegister={register('username')}
+                    inputLabel="Username"
+                />
 
-        <PasswordInputField
-          separateLabel
-          errors={formState.errors}
-          inputLabel="Password"
-          useFormRegister={register('password')}
-        />
+                <PasswordInputField
+                    separateLabel
+                    errors={formState.errors}
+                    formRegister={register('password')}
+                    inputLabel="Password"
+                />
 
-        <Button className="login-button" loading={userState.loading} type="submit">
-          Login
-        </Button>
+                {userState.error.code ? (
+                    <InputFieldWrapper>
+                        <Alert severity="error" variant="filled">
+                            <AlertTitle>{userState.error.code.toUpperCase()}</AlertTitle>
+                            {userState.error.message}
+                        </Alert>
+                    </InputFieldWrapper>
+                ) : null}
 
-        <Separator />
+                <Button className="login-button" loading={userState.loading} type="submit">
+                    Login
+                </Button>
 
-        <p>
-          Don&#39;t have an account? Click on <span className="register-link">Register</span> button
-          below
-        </p>
+                <Separator />
 
-        <Link to="/register">
-          <Button color="success">Register</Button>
-        </Link>
-      </form>
-    </div>
-  );
+                <p>
+                    Don&#39;t have an account ? Click on
+                    {/* TODO: Find alternative way to remove default link style like blue underline */}
+                    <Link style={{ textDecoration: 'none' }} to="/register">
+                        <span className="register-link"> Register </span>
+                    </Link>
+                    button below
+                </p>
+
+                <Link style={{ textDecoration: 'none' }} to="/register">
+                    <Button color="success">Register</Button>
+                </Link>
+            </form>
+        </div>
+    );
 };
 
 export default LoginPage;

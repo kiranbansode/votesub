d7ba4a4844c2dfd470e80efc68e30a585f1181b6
kiranbansode <kiran5120135@gmail.com>
2022-06-01 22:12:54 +0530

    style(Components): change tab size to 4 from 2

--- src/components/DateInputField/index.tsx
+++ src/components/DateInputField/index.tsx
@@ -1,76 +1,81 @@
-import { useState } from 'react';
-import { UseFormRegisterReturn } from 'react-hook-form';
+import { Control, Controller, FieldErrors } from 'react-hook-form';
 import DateAdapter from '@mui/lab/AdapterDateFns';
 import { TextField, FormControl, FormHelperText } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/lab';
 import InputFieldWrapper from 'styled/InputFieldWrapper';
 import SeparateLabel from 'components/SeparateLabel';
+import inputErrorMessageFinder from 'utils/helperFunctions/inputErrorMessageFinder';
 
-interface DateInputFieldTypes {
-  useFormRegister: UseFormRegisterReturn;
-  inputLabel?: string;
-  inputHelperText?: string;
-  separateLabel?: boolean;
-  noLabel?: boolean;
+interface IDateInputField {
+    inputErrors: FieldErrors;
+    fieldName: string;
+    control: Control;
+    inputLabel: string;
+    inputHelperText?: string;
+    separateLabel?: boolean;
+    noLabel?: boolean;
 }
 
 const DateInputField = ({
-  separateLabel,
-  noLabel,
-  inputHelperText,
-  inputLabel,
-  useFormRegister,
-}: DateInputFieldTypes) => {
-  const [value, setValue] = useState<Date | null>(new Date());
+    inputErrors,
+    separateLabel,
+    noLabel,
+    fieldName,
+    inputHelperText,
+    inputLabel,
+    control,
+}: IDateInputField) => {
+    const error = inputErrorMessageFinder(fieldName, inputErrors);
+    let labelToShow: string | null;
 
-  let labelToShow;
+    if (!noLabel) {
+        /* Depending on passed conditon it will do one of follwing
+            1. Separate Label - Detached from TextInputField
+            2. No Label - Label will not be showed
+            3. Default - Label will be shown in top border of TextInputField
+        */
+        labelToShow = !separateLabel ? inputLabel : null;
+    }
 
-  if (!noLabel) {
-    /* Depending on passed conditon it will do one of follwing
-    1. Separate Label - Detached from TextInputField
-    2. No Label - Label will not be showed
-    3. Default - Label will be shown in top border of TextInputField
-  */
-    labelToShow = !separateLabel ? inputLabel : null;
-  }
+    return (
+        <>
+            <InputFieldWrapper>
+                <FormControl fullWidth error={Boolean(error)}>
+                    {separateLabel ? (
+                        <SeparateLabel htmlFor={fieldName} label={inputLabel} />
+                    ) : null}
 
-  return (
-    <>
-      <InputFieldWrapper>
-        <FormControl>
-          {separateLabel ? (
-            <SeparateLabel htmlFor={useFormRegister.name} label={inputLabel} />
-          ) : null}
-
-          <LocalizationProvider
-            dateAdapter={DateAdapter}
-            dateFormats={{ keyboardDate: 'dd/MM/yyyy' }}
-          >
-            <DatePicker
-              disableFuture
-              label={labelToShow}
-              openTo="day"
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              renderInput={(params) => <TextField {...params} />}
-              value={value}
-              views={['year', 'month', 'day']}
-              onChange={(newValue) => {
-                setValue(newValue);
-              }}
-            />
-          </LocalizationProvider>
-          <FormHelperText id={useFormRegister.name}>{inputHelperText}</FormHelperText>
-        </FormControl>
-      </InputFieldWrapper>
-    </>
-  );
+                    <Controller
+                        control={control}
+                        name={fieldName}
+                        render={({ field }) => (
+                            <LocalizationProvider
+                                dateAdapter={DateAdapter}
+                                dateFormats={{ keyboardDate: 'dd/MM/yyyy' }}
+                            >
+                                <DatePicker
+                                    disableFuture
+                                    label={labelToShow}
+                                    openTo="day"
+                                    // eslint-disable-next-line react/jsx-props-no-spreading
+                                    renderInput={(params) => <TextField {...params} />}
+                                    // eslint-disable-next-line react/jsx-props-no-spreading
+                                    {...field}
+                                />
+                            </LocalizationProvider>
+                        )}
+                    />
+                    <FormHelperText id={fieldName}>{error || inputHelperText}</FormHelperText>
+                </FormControl>
+            </InputFieldWrapper>
+        </>
+    );
 };
 
 DateInputField.defaultProps = {
-  separateLabel: false,
-  noLabel: false,
-  inputHelperText: '',
-  inputLabel: 'DateInputField',
+    separateLabel: false,
+    noLabel: false,
+    inputHelperText: 'DateInputField',
 };
 
 export default DateInputField;

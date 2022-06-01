ccd6cf374544cabecf07abf7d596f21439c7b633
kiranbansode <kiran5120135@gmail.com>
2022-06-02 01:11:49 +0530

    style: change tab size to 4 from 2

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,21 @@ const RegistrationPage = lazy(() => import(`./pages/RegistrationPage`));
 const DashboardPage = lazy(() => import(`./pages/DashboardPage`));
 
 function App() {
-  return (
-    <div className="App">
-      {/* <p style={{ textAlign: 'center', color: 'red', margin: '0' }}>Only For Mobiles</p>
+    return (
+        <div className="App">
+            {/* <p style={{ textAlign: 'center', color: 'red', margin: '0' }}>Only For Mobiles</p>
       <p style={{ textAlign: 'center', margin: '0', fontSize: '10px' }}> Try on a mobile device</p> */}
-      <Suspense fallback={<CircularProgress />}>
-        <BrowserRouter>
-          <Routes>
-            <Route element={<LoginPage />} path="/" />
-            <Route element={<RegistrationPage />} path="/register" />
-            <Route element={<DashboardPage />} path="/dashboard" />
-          </Routes>
-        </BrowserRouter>
-      </Suspense>
-    </div>
-  );
+            <Suspense fallback={<CircularProgress />}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<LoginPage />} path="/" />
+                        <Route element={<RegistrationPage />} path="/register" />
+                        <Route element={<DashboardPage />} path="/dashboard" />
+                    </Routes>
+                </BrowserRouter>
+            </Suspense>
+        </div>
+    );
 }
 
 export default App;

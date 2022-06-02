29f00b6194c46be39ed42d1e015b4c43b31ac149
kiranbansode <kiran5120135@gmail.com>
2022-06-02 18:00:47 +0530

    feat(functions): add createNewUserCLF function
    This function will create new user using Cloud Functions Admin API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,24 @@
-export default {};
+import * as cloudFunctions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const clf = cloudFunctions.region('asia-south1');
+
+admin.initializeApp();
+
+exports.createNewUser = clf.https.onCall(async (data) => {
+    const response = await admin.auth().createUser({
+        email: data.emailId,
+        password: data.password,
+    });
+    return response;
+});
+
+// exports.sendUserVerificationEmail = clf.auth.user().onCreate(async (user) => {
+//     const { emailVerified, email } = user;
+//     if (!emailVerified) {
+//         return;
+//     }
+
+//     const link = await admin.auth().generateEmailVerificationLink(email!);
+//     console.log(link);
+// });

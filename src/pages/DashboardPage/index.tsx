310a938209bee08d101bbdde80003c2e514e0ebc
kiranbansode <kiran5120135@gmail.com>
2022-08-02 22:39:21 +0530

    feat: user will now see some dummy voting candidates
    This will help in future for developing frontend
    as well as backend functionality

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,5 +1,6 @@
 import Header from 'components/Header';
 import Separator from 'components/Separator';
+import Candidate from 'components/Candidate';
 
 import './Dashboard.styles.scss';
 
@@ -10,6 +11,20 @@ const DashboardPage = () => (
             Your Remaining Votes : <span>100</span>
         </p>
         <Separator />
+
+        <p className="voting-topic"> -x- Most Favourite Subject -x-</p>
+
+        <div id="candidate-list">
+            <Candidate candidateName="Geography" position={1} totalVotes={1995} />
+
+            <Candidate candidateName="History" position={2} totalVotes={1994} />
+
+            <Candidate candidateName="Mathematics" position={3} totalVotes={1993} />
+
+            <Candidate candidateName="English" position={4} totalVotes={1992} />
+
+            <Candidate candidateName="Science" position={5} totalVotes={1991} />
+        </div>
     </div>
 );
 

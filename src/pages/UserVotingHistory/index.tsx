b427a8f4167415ca82c7e77ca00df13e033e7c26
kiranbansode <kiran5120135@gmail.com>
2022-08-04 23:05:52 +0530

    feat: add UserVotingHistory page to pages
    This page will let users to see thier voting history

---
+++ src/pages/UserVotingHistory/index.tsx
@@ -0,0 +1,10 @@
+import Header from 'components/Header';
+import './UserVotingHistory.style.scss';
+
+const UserVotingHistory = () => (
+    <div>
+        <Header />
+    </div>
+);
+
+export default UserVotingHistory;

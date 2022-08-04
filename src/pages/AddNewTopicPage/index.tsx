a548359f5bbc4359a7c496bc6ab365c533cff143
kiranbansode <kiran5120135@gmail.com>
2022-08-04 23:04:40 +0530

    feat: add AddNEwTopicPage to pages
    This page will let user to add new topics for voting

---
+++ src/pages/AddNewTopicPage/index.tsx
@@ -0,0 +1,10 @@
+import Header from 'components/Header';
+import './AddNewTopicPage.style.scss';
+
+const AddNewTopicPage = () => (
+    <div>
+        <Header />
+    </div>
+);
+
+export default AddNewTopicPage;

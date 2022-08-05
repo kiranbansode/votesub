487fae3f05927ac0fe94c1b2973e2bcdb79e9938
kiranbansode <kiran5120135@gmail.com>
2022-08-05 23:58:19 +0530

    feat: now use can close sidebar by clicking close icon
    fun fact: implemented within 7 min

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,37 +1,51 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
 import { Link } from 'react-router-dom';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import AddIcon from '@mui/icons-material/Add';
 import HistoryIcon from '@mui/icons-material/History';
-import './SideBar.styles.scss';
+import CloseIcon from '@mui/icons-material/Close';
 import Separator from 'components/Separator';
 
-const SideBar = () => (
-    <div id="sidebar">
-        <ul>
-            <Separator />
-            <li>
-                <Link to="/dashboard">
-                    <DashboardIcon />
-                    <span> Dashboard</span>
-                </Link>
-            </li>
-            <Separator />
-            <li>
-                <Link to="/addNewTopic">
-                    <AddIcon />
-                    <span>Add New Topic</span>
-                </Link>
-            </li>
-            <Separator />
-            <li>
-                <Link to="/userVotingHistory">
-                    <HistoryIcon />
-                    <span>Voting History</span>
-                </Link>
-            </li>
-            <Separator />
-        </ul>
-    </div>
-);
+import './SideBar.styles.scss';
+
+const SideBar = () => {
+    const handleShowSidebar = () => {
+        const closeIcon = document.querySelector('#sidebar')! as HTMLElement;
+        closeIcon.style.display = 'none';
+    };
+    return (
+        <div id="sidebar">
+            <ul>
+                <span className="close-sidebar-icon" onClick={() => handleShowSidebar()}>
+                    <CloseIcon />
+                </span>
+                <Separator />
+                <li>
+                    <Link to="/dashboard">
+                        <DashboardIcon />
+                        <span> Dashboard</span>
+                    </Link>
+                </li>
+                <Separator />
+                <li>
+                    <Link to="/addNewTopic">
+                        <AddIcon />
+                        <span>Add New Topic</span>
+                    </Link>
+                </li>
+                <Separator />
+                <li>
+                    <Link to="/userVotingHistory">
+                        <HistoryIcon />
+                        <span>Voting History</span>
+                    </Link>
+                </li>
+                <Separator />
+            </ul>
+        </div>
+    );
+};
 
 export default SideBar;

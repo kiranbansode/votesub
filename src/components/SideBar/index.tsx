c80632ceda9dae351cfb9ca144e958b52401bee5
kiranbansode <kiran5120135@gmail.com>
2022-08-07 01:36:35 +0530

    refactor: optimize Sidebar components
    This optimization include reduce in code size

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 
@@ -6,42 +7,33 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import AddIcon from '@mui/icons-material/Add';
 import HistoryIcon from '@mui/icons-material/History';
 import CloseIcon from '@mui/icons-material/Close';
-import Separator from 'components/Separator';
 import useAppDispatch from 'hooks/useAppDispatch';
 import { HIDE_SIDEBAR } from 'store/globalUIState';
 
 import './SideBar.styles.scss';
+
+const linksToPages = [
+    { link: '', linkIcon: <CloseIcon />, linkName: '' },
+    { link: '/dashboard', linkIcon: <DashboardIcon />, linkName: 'Dashboard' },
+    { link: '/addNewTopic', linkIcon: <AddIcon />, linkName: 'Add New Topic' },
+    { link: '/userVotingHistory', linkIcon: <HistoryIcon />, linkName: 'Voting History' },
+];
 
 const SideBar = () => {
     const dispatch = useAppDispatch();
     return (
         <div id="sidebar">
             <ul>
-                <span className="close-sidebar-icon" onClick={() => dispatch(HIDE_SIDEBAR())}>
-                    <CloseIcon />
-                </span>
-                <Separator />
-                <li>
-                    <Link to="/dashboard">
-                        <DashboardIcon />
-                        <span> Dashboard</span>
-                    </Link>
-                </li>
-                <Separator />
-                <li>
-                    <Link to="/addNewTopic">
-                        <AddIcon />
-                        <span>Add New Topic</span>
-                    </Link>
-                </li>
-                <Separator />
-                <li>
-                    <Link to="/userVotingHistory">
-                        <HistoryIcon />
-                        <span>Voting History</span>
-                    </Link>
-                </li>
-                <Separator />
+                {linksToPages.map((page) => (
+                    <>
+                        <li key={page.link} onClick={() => dispatch(HIDE_SIDEBAR())}>
+                            <Link to={page.link}>
+                                {page.linkIcon}
+                                <span>{page.linkName}</span>
+                            </Link>
+                        </li>
+                    </>
+                ))}
             </ul>
         </div>
     );

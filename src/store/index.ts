0ba4bdd6f667de40dc295a1b4f846e01d6aeca6f
kiranbansode <kiran5120135@gmail.com>
2022-08-08 23:47:58 +0530

    refactor(store): rename globalUIState to ui

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterSlice from 'store/counterSlice';
 import userLoginSlice from 'store/loginPage/userLoginSlice';
 import createNewUserSlice from 'store/registrationPage/createNewUserSlice';
-import globalUIState from 'store/globalUIState';
+import uiSlice from 'store/ui';
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice,
         user: userLoginSlice,
         registration: createNewUserSlice,
-        globalUI: globalUIState,
+        ui: uiSlice,
     },
 });
 

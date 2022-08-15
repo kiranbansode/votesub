48761b27fc2200791d7c66a88eb3575096a4b529
kiranbansode <kiran5120135@gmail.com>
2022-08-15 23:49:28 +0530

    feat(Button): add onClick prop to Button Component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,39 +3,51 @@ import { LoadingButton as LoadingButtonMaterial, LoadingButtonProps } from '@mui
 import InputFieldWrapper from 'styled/InputFieldWrapper';
 
 interface ButtonPropsType {
-  children?: ButtonProps['children'];
-  color?: ButtonProps['color'];
-  loading?: LoadingButtonProps['loading'];
-  size?: ButtonProps['size'];
-  type?: 'reset' | 'submit' | 'button';
-  variant?: ButtonProps['variant'];
-  className?: string;
+    children?: ButtonProps['children'];
+    color?: ButtonProps['color'];
+    loading?: LoadingButtonProps['loading'];
+    size?: ButtonProps['size'];
+    type?: 'reset' | 'submit' | 'button';
+    variant?: ButtonProps['variant'];
+    className?: string;
+    onClick?: () => void;
 }
 
-const Button = ({ color, variant, children, size, type, loading, className }: ButtonPropsType) => (
-  <InputFieldWrapper>
-    <LoadingButtonMaterial
-      fullWidth
-      className={className}
-      color={color}
-      loading={loading}
-      size={size}
-      type={type}
-      variant={variant}
-    >
-      {children}
-    </LoadingButtonMaterial>
-  </InputFieldWrapper>
+const Button = ({
+    color,
+    variant,
+    children,
+    size,
+    type,
+    loading,
+    className,
+    onClick,
+}: ButtonPropsType) => (
+    <InputFieldWrapper>
+        <LoadingButtonMaterial
+            fullWidth
+            className={className}
+            color={color}
+            loading={loading}
+            size={size}
+            type={type}
+            variant={variant}
+            onClick={onClick}
+        >
+            {children}
+        </LoadingButtonMaterial>
+    </InputFieldWrapper>
 );
 
 Button.defaultProps = {
-  children: 'Button',
-  className: '',
-  color: 'primary',
-  loading: false,
-  size: 'large',
-  type: 'button',
-  variant: 'contained',
+    children: 'Button',
+    className: '',
+    color: 'primary',
+    loading: false,
+    size: 'large',
+    type: 'button',
+    variant: 'contained',
+    onClick: () => {},
 };
 
 export default Button;

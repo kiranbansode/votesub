9ba9e209f5b4508d34317e46bd1d8b96f167c2c1
kiranbansode <kiran5120135@gmail.com>
2022-08-16 14:37:08 +0530

    feat: add toProperCase to helperFunctions
    This function will return properCase to any passed strings

---
+++ src/utils/helperFunctions/toProperCase.ts
@@ -0,0 +1,10 @@
+const toProperCase = (str: string) => {
+    const strGroup = str.toLowerCase().split(' ');
+    const result = strGroup.map(
+        (singleStr) => singleStr.charAt(0).toUpperCase() + singleStr.slice(1),
+    );
+
+    return result.join(' ');
+};
+
+export default toProperCase;

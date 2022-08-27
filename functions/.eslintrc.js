93751197b9fd80a2b86405f75a7594ea2e3e583e
kiranbansode <kiran5120135@gmail.com>
2022-08-27 23:35:51 +0530

    build: export rules from .eslintrc.json files to .eslintrc.js
    Was having error while parsing tsconfig.json, so it was needed
    to export rules to .eslintrc.js

---
+++ functions/.eslintrc.js
@@ -0,0 +1,32 @@
+module.export = {
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        'plugin:import/typescript',
+        'google',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.dev.json'],
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+    },
+    ignorePatterns: [
+        '/lib/**/*', // Ignore built files.
+    ],
+    plugins: ['@typescript-eslint', 'import'],
+    rules: {
+        quotes: ['error', 'single'],
+        indent: ['error', 4],
+        'object-curly-spacing': ['error', 'always'],
+        'max-len': ['error', 100],
+        'import/no-unresolved': 0,
+    },
+};

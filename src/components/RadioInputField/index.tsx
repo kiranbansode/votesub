4c4d6173ee7916f9be92a2d00c574aeffcd98043
kiranbansode <kiran5120135@gmail.com>
2022-08-30 22:45:07 +0530

    feat(RadioInputField): add more features to RadioInputField components
    From now on RadioInputField will need array of object which contains
    label and value which will serve as individaul radio input

--- src/components/RadioInputField/index.tsx
+++ src/components/RadioInputField/index.tsx
@@ -5,52 +5,70 @@ import InputFieldWrapper from 'styled/InputFieldWrapper';
 
 import './RadioInputField.styles.scss';
 
+interface IRadioSelect {
+    label: string;
+    value: string;
+}
+
 interface RadioInputFieldTypes {
-  useFormControl: Control;
-  fieldName: string;
-  inputLabel: string;
-  inputHelperText?: string;
-  separateLabel?: boolean;
+    useFormControl: Control;
+    fieldName: string;
+    inputLabel: string;
+    inputHelperText?: string;
+    separateLabel?: boolean;
+    radioSelect: IRadioSelect[];
 }
 
 const RadioInputField = ({
-  useFormControl,
-  fieldName,
-  inputLabel,
-  inputHelperText,
-  separateLabel,
+    useFormControl,
+    fieldName,
+    inputLabel,
+    inputHelperText,
+    separateLabel,
+    radioSelect,
 }: RadioInputFieldTypes) => (
-  <Controller
-    control={useFormControl}
-    name={fieldName}
-    render={({ field }) => (
-      <>
-        <InputFieldWrapper id="radio-buttons">
-          <FormControl>
-            {separateLabel ? <SeparateLabel htmlFor={field.name} label={inputLabel} /> : null}
-            <RadioGroup
-              row
-              aria-labelledby="gender"
-              className="radio-buttons"
-              name={field.name}
-              value={field.value || 'male'}
-              onBlur={field.onBlur}
-              onChange={field.onChange}
-            >
-              <FormControlLabel control={<Radio />} label="Male" value="male" />
-              <FormControlLabel control={<Radio />} label="Female" value="female" />
-            </RadioGroup>
-            <FormHelperText>{inputHelperText}</FormHelperText>
-          </FormControl>
-        </InputFieldWrapper>
-      </>
-    )}
-  />
+    <Controller
+        control={useFormControl}
+        name={fieldName}
+        render={({ field }) => (
+            <>
+                <InputFieldWrapper id="radio-buttons">
+                    <FormControl>
+                        {separateLabel ? (
+                            <SeparateLabel htmlFor={field.name} label={inputLabel} />
+                        ) : null}
+                        <RadioGroup
+                            row
+                            // aria-labelledby="gender"
+                            className="radio-buttons"
+                            name={field.name}
+                            value={field.value || 'male'}
+                            onBlur={field.onBlur}
+                            onChange={field.onChange}
+                        >
+                            {radioSelect.map(({ label, value }) => (
+                                <FormControlLabel
+                                    control={<Radio />}
+                                    key={label}
+                                    label={label}
+                                    value={value}
+                                />
+                            ))}
+
+                            {/* <FormControlLabel control={<Radio />} label="Male" value="male" />
+                            <FormControlLabel control={<Radio />} label="Female" value="female" /> */}
+                        </RadioGroup>
+                        <FormHelperText>{inputHelperText}</FormHelperText>
+                    </FormControl>
+                </InputFieldWrapper>
+            </>
+        )}
+    />
 );
 
 RadioInputField.defaultProps = {
-  inputHelperText: '',
-  separateLabel: true,
+    inputHelperText: '',
+    separateLabel: true,
 };
 
 export default RadioInputField;

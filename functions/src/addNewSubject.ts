60716ee6a16a58125e35da035b0c3acd1aca694e
kiranbansode <kiran5120135@gmail.com>
2022-09-02 23:59:18 +0530

    feat(cloudfn): add more functopm

--- functions/src/addNewSubject.ts
+++ functions/src/addNewSubject.ts
@@ -2,18 +2,60 @@
 import * as admin from 'firebase-admin';
 import clf from './index';
 
-exports.addNewSubject = clf.https.onCall(async (data, context) => {
-    try {
-        const response = await admin
-            .firestore()
-            .collection('subjects')
-            .doc(data.id)
-            .set({
-                ...data,
-            });
+interface ICandidate {
+    id: string;
+    candidateName: string;
+}
+interface IAddNewSubject {
+    id: string;
+    subject: string;
+    submittedBy: string;
+    userId: string;
+    candidates: ICandidate[];
+}
 
-        console.log(response);
+exports.addNewSubject = clf.https.onCall(async (data: IAddNewSubject, context) => {
+    console.log(data, context.auth);
+
+    try {
+        // eslint-disable-next-line no-use-before-define
+        const subjRes = await saveSubjectDetails(data);
+        // eslint-disable-next-line no-use-before-define
+        const candiRes = await saveCandidateDetails(data.candidates);
+        console.log(subjRes, candiRes);
     } catch (error) {
         console.log(error);
     }
 });
+
+function saveSubjectDetails({
+    id,
+    candidates,
+    subject,
+    submittedBy,
+    userId,
+}: IAddNewSubject): Promise<any> {
+    return admin
+        .firestore()
+        .collection('subjects')
+        .doc(id)
+        .set({
+            id,
+            subject,
+            submittedBy,
+            userId,
+            // eslint-disable-next-line no-shadow
+            candidates: candidates.map(({ id }) => id),
+        });
+}
+
+function saveCandidateDetails(candidates: ICandidate[]) {
+    console.log(candidates);
+
+    return candidates.map(({ id, candidateName }) =>
+        admin.firestore().collection('candidates').doc(id).set({
+            id,
+            candidateName,
+        }),
+    );
+}

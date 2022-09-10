8921aabc9287331fb88150f7841bbdc96243feb7
kiranbansode <kiran5120135@gmail.com>
2022-09-11 00:24:45 +0530

    feat: add options for dev and hr

---
+++ src/utils/menuOptions/developer.ts
@@ -0,0 +1,13 @@
+import { IOptions } from '.';
+
+const devOptions: IOptions[] = [
+    { value: 'int', option: 'Intern' },
+    { value: 'jr', option: 'Junior Engineer' },
+    { value: 'md', option: 'Mid-Level Engineer' },
+    { value: 'sr', option: 'Senior Engineer' },
+    { value: 'stf', option: 'Staff Engineer' },
+    { value: 'pr', option: 'Principal Engineer' },
+    { value: 'tf', option: 'Technical Fellow' },
+];
+
+export default devOptions;

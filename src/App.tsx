8b66b2f52ff6a00eb8d14085caf9f4ebfa039e0e
kiranbansode <kiran5120135@gmail.com>
2022-09-11 13:33:27 +0530

    feat(RegistrationPage): add nested routes
    Following nested routes were added to navigate user
    to their registration form
        - student: StudentRegForm
        - teacher: TeacherRegForm
        - employer: EmployerRegForm
        - developer: DeveloperRegForm

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-const LoginPage = lazy(() => import(`./pages/LoginPage`));
-const RegistrationPage = lazy(() => import(`./pages/RegistrationPage`));
-const DashboardPage = lazy(() => import(`./pages/DashboardPage`));
-const AddNewTopicPage = lazy(() => import(`./pages/AddNewTopicPage`));
-const UserVotingHistory = lazy(() => import(`./pages/UserVotingHistory`));
-const FeedbackPage = lazy(() => import(`./pages/FeedbackPage`));
-const CreditsPage = lazy(() => import(`./pages/CreditsPage`));
-const AboutMePage = lazy(() => import(`./pages/AboutMePage`));
-const AdminPage = lazy(() => import(`./pages/AdminPage`));
-const SettingsPage = lazy(() => import(`./pages/SettingsPage`));
+// Main Page Routes
+const LoginPage = lazy(() => import(`pages/LoginPage`));
+const RegistrationPage = lazy(() => import(`pages/RegistrationPage`));
+const DashboardPage = lazy(() => import(`pages/DashboardPage`));
+const AddNewTopicPage = lazy(() => import(`pages/AddNewTopicPage`));
+const UserVotingHistory = lazy(() => import(`pages/UserVotingHistory`));
+const FeedbackPage = lazy(() => import(`pages/FeedbackPage`));
+const CreditsPage = lazy(() => import(`pages/CreditsPage`));
+const AboutMePage = lazy(() => import(`pages/AboutMePage`));
+const AdminPage = lazy(() => import(`pages/AdminPage`));
+const SettingsPage = lazy(() => import(`pages/SettingsPage`));
+
+// Registration Page Nested Routes
+const StudentRegForm = lazy(() => import('pages/RegistrationPage/StudentRegForm'));
+const DeveloperRegForm = lazy(() => import('pages/RegistrationPage/DeveloperRegForm'));
+const EmployerRegForm = lazy(() => import('pages/RegistrationPage/EmployerRegForm'));
+const TeacherRegForm = lazy(() => import('pages/RegistrationPage/TeacherRegForm'));
 
 function App() {
     return (
@@ -35,7 +42,11 @@ function App() {
                 <BrowserRouter>
                     <Routes>
                         <Route element={<LoginPage />} path="/" />
-                        <Route element={<RegistrationPage />} path="/register" />
+                        <Route element={<RegistrationPage />} path="/register/" />
+                        <Route element={<StudentRegForm />} path="/register/student" />
+                        <Route element={<TeacherRegForm />} path="/register/teacher" />
+                        <Route element={<EmployerRegForm />} path="/register/employer" />
+                        <Route element={<DeveloperRegForm />} path="/register/developer" />
                         <Route element={<DashboardPage />} path="/dashboard" />
                         <Route element={<AddNewTopicPage />} path="/addNewTopic" />
                         <Route element={<UserVotingHistory />} path="/userVotingHistory" />
--- src/pages/RegistrationPage/index.tsx
+++ src/pages/RegistrationPage/index.tsx
@@ -1,44 +1,20 @@
-import { lazy } from 'react';
+import { lazy, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useForm, FieldValues } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { standardOptions } from 'utils/menuOptions/standards';
-import { divisionOptions } from 'utils/menuOptions/divisions';
 import useAppDispatch from 'hooks/useAppDispatch';
 import useAppSelector from 'hooks/useAppSelector';
-import { createNewUserThunk } from 'store/registrationPage/createNewUserSlice';
 import { SAVE_USER_ROLE } from 'store/registrationPage/saveuserRoleSlice';
 
-// @ts-ignore
-import registrationFormValidation from './yupValidation.ts';
 import './RegistrationPage.styles.scss';
 
-const TextInputField = lazy(() => import('components/TextInputField'));
-const PasswordInputField = lazy(() => import('components/PasswordInputField'));
 const Button = lazy(() => import('components/Button'));
 const RadioInputField = lazy(() => import('components/RadioInputField'));
-const DateInputField = lazy(() => import('components/DateInputField'));
-const SelectInputField = lazy(() => import('components/SelectInputField'));
 const Separator = lazy(() => import('components/Separator'));
 const Logo = lazy(() => import('components/Logo'));
 const Caption = lazy(() => import('components/Caption'));
 
 const REGISTRATION_FORM_DEFAULT_VALUE = {
-    // name: {
-    //     firstName: '',
-    //     middleName: '',
-    //     lastName: '',
-    // },
-    // gender: 'male',
-    // dob: new Date(),
-    // schoolName: '',
-    // std: '',
-    // div: '',
-    // mobileNo: '',
-    // altMobileNo: '',
-    // emailId: '',
-    // password: '',
-    // confirmPassword: '',
-    role: 'st',
+    role: '',
 };
 
 const RegistrationPage = () => {
@@ -46,7 +22,27 @@ const RegistrationPage = () => {
         defaultValues: REGISTRATION_FORM_DEFAULT_VALUE,
         // resolver: yupResolver(registrationFormValidation),
     });
+    const navigate = useNavigate();
+    const location = useLocation();
     const dispatch = useAppDispatch();
+    const userRole = useAppSelector((state) => state.userRole.role);
+
+    const REG_NESTED_ROUTES = {
+        st: 'student',
+        tr: 'teacher',
+        hr: 'employer',
+        dev: 'developer',
+    };
+
+    useEffect(() => {
+        if (userRole) {
+            navigate(
+                `${location.pathname}/${
+                    REG_NESTED_ROUTES[userRole as keyof typeof REG_NESTED_ROUTES]
+                }`,
+            );
+        }
+    }, [userRole]);
 
     return (
         <div className="page" id="registration-page">
@@ -60,7 +56,6 @@ const RegistrationPage = () => {
 
             <form
                 onSubmit={handleSubmit((data) => {
-                    console.log('Submitting');
                     dispatch(SAVE_USER_ROLE(data));
                 })}
             >
@@ -78,108 +73,6 @@ const RegistrationPage = () => {
                 />
 
                 <Button type="submit">Submit</Button>
-                <button type="submit">Submit</button>
-
-                {/* <TextInputField
-                    autoFocus
-                    separateLabel
-                    errors={formState.errors}
-                    formRegister={register('name.firstName')}
-                    inputLabel="Name"
-                    inputPlaceholder="Your Name"
-                />
-
-                <TextInputField
-                    noLabel
-                    errors={formState.errors}
-                    formRegister={register('name.middleName')}
-                    inputPlaceholder="Father/Husband Name"
-                />
-
-                <TextInputField
-                    noLabel
-                    errors={formState.errors}
-                    formRegister={register('name.lastName')}
-                    inputPlaceholder="Surname"
-                />
-
-                <RadioInputField
-                    fieldName="gender"
-                    inputLabel="Gender"
-                    radioSelect={[
-                        { label: 'Male', value: 'male' },
-                        { label: 'Female', value: 'female' },
-                    ]}
-                    useFormControl={control}
-                />
-
-                <DateInputField
-                    separateLabel
-                    control={control}
-                    fieldName="dob"
-                    inputErrors={formState.errors}
-                    inputLabel="Date of Birth"
-                />
-
-                <TextInputField
-                    separateLabel
-                    errors={formState.errors}
-                    formRegister={register('schoolName')}
-                    inputLabel="School Name"
-                />
-
-                <SelectInputField
-                    showSeparateLabel
-                    control={control}
-                    fieldName="std"
-                    inputErrors={formState.errors}
-                    inputLabel="Standard"
-                    options={standardOptions}
-                />
-
-                <SelectInputField
-                    showSeparateLabel
-                    control={control}
-                    fieldName="div"
-                    inputErrors={formState.errors}
-                    inputLabel="Division"
-                    options={divisionOptions}
-                />
-
-                <TextInputField
-                    separateLabel
-                    errors={formState.errors}
-                    formRegister={register('mobileNo')}
-                    inputLabel="Mobile No. (WhatsApp)"
-                />
-
-                <TextInputField
-                    separateLabel
-                    errors={formState.errors}
-                    formRegister={register('altMobileNo')}
-                    inputLabel="Alternate Mobile No."
-                />
-
-                <TextInputField
-                    separateLabel
-                    errors={formState.errors}
-                    formRegister={register('emailId')}
-                    inputLabel="Email ID"
-                />
-
-                <PasswordInputField
-                    separateLabel
-                    errors={formState.errors}
-                    formRegister={register('password')}
-                    inputLabel="Password"
-                />
-
-                <PasswordInputField
-                    separateLabel
-                    errors={formState.errors}
-                    formRegister={register('confirmPassword')}
-                    inputLabel="Confirm Your Password"
-                /> */}
             </form>
         </div>
     );

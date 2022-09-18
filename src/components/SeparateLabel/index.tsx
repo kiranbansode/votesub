c539851a07a3d28cc827cf9231d167e6f1a5c1ad
kiranbansode <kiran5120135@gmail.com>
2022-09-18 20:20:13 +0530

    feat(SeparateLabel): add required prop
    If true it will show red asteric at label's end

--- src/components/SeparateLabel/index.tsx
+++ src/components/SeparateLabel/index.tsx
@@ -1,18 +1,21 @@
 import './SeparareLabel.styles.scss';
 
 interface SeparateLabelBaseProps {
-  htmlFor: string;
-  label?: string;
+    htmlFor: string;
+    label?: string;
+    required?: boolean;
 }
 
-const SeparateLabel = ({ htmlFor, label }: SeparateLabelBaseProps) => (
-  <label className="separate-label" htmlFor={htmlFor}>
-    {label}
-  </label>
+const SeparateLabel = ({ htmlFor, label, required }: SeparateLabelBaseProps) => (
+    <label className="separate-label" htmlFor={htmlFor}>
+        {label}
+        {required ? <span className="required-star">*</span> : null}
+    </label>
 );
 
 SeparateLabel.defaultProps = {
-  label: 'Separate Label',
+    label: 'Separate Label',
+    required: false,
 };
 
 export default SeparateLabel;

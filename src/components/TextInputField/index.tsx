7cc29ad9c16fa0cedd67798548a61afb3ed28832
kiranbansode <kiran5120135@gmail.com>
2022-10-04 14:26:18 +0530

    refactor(TextInputField): spelling mistakes

--- src/components/TextInputField/index.tsx
+++ src/components/TextInputField/index.tsx
@@ -7,11 +7,11 @@ import './TextInputField.styles.scss';
 
 /**
  * TextInputField is a wrapper component around Material-UI's TextField.
- * It has much more capabilities like intergration with React-Hook-Form,
- * custom Error Messages and more.
+ * It has much more capabilities like integration with React-Hook-Form,
+ * custom Error Messages and many more.
  *
  * @author Kiran A. Bansode <kiran5120135@gmail.com> <czar.kiran@gmail.com>
- * @return TextField Componenet
+ * @return TextField Component
  */
 
 function TextInputField({
@@ -29,7 +29,7 @@ function TextInputField({
 
     let labelToShow;
     /*
-     * Depending on passed conditon it will do one of follwing
+     * Depending on passed condition it will do one of following
      * Separate Label - Detached from TextInputField
      * No Label - Label will not be showed
      * Default - Label will be shown in top border of TextInputField like legend of a fieldset tag

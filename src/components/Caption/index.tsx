7f9c03be9537a142c6cc2cbe7f3b750cbd2983d8
kiranbansode <kiran5120135@gmail.com>
2022-10-04 14:08:28 +0530

    fix(Caption): fix some styling issues

--- src/components/Caption/index.tsx
+++ src/components/Caption/index.tsx
@@ -3,9 +3,9 @@ import VoteSubCaption from 'assets/svg/caption.votesub.svg';
 import './Caption.styles.scss';
 
 const Caption = () => (
-  <div className="caption" id="votesub-caption">
-    <img alt="votesub-caption" src={VoteSubCaption} />
-  </div>
+    <div className="votesub-caption-container">
+        <img alt="votesub-caption" className="votesub-caption" src={VoteSubCaption} />
+    </div>
 );
 
 export default Caption;

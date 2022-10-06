8531695813163f44526eef28632b56dd17872364
kiranbansode <kiran5120135@gmail.com>
2022-10-06 20:48:34 +0530

    refactor(cloudFn): improve code readability

--- functions/src/createNewUser.ts
+++ functions/src/createNewUser.ts
@@ -1,47 +1,33 @@
 /* eslint-disable import/extensions */
-import * as admin from 'firebase-admin';
 import { HttpsError } from 'firebase-functions/v1/auth';
 import clf from './index';
 
-exports.createNewUser = clf.https.onCall(async (data) => {
-    const {
-        name,
-        gender,
-        dob,
-        schoolName,
-        std,
-        div,
-        mobileNo,
-        altMobileNo,
-        emailId,
-        password,
-        role,
-    } = data;
-
+exports.createNewUser = clf.https.onCall(async (newUserData) => {
+    const { auth, firestore } = await import('firebase-admin');
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, confirmPassword, ...otherNewUserData } = newUserData;
     try {
-        const response = await admin.auth().createUser({
-            email: emailId,
-            password,
+        const { uid } = await auth().createUser({
+            email: newUserData.emailId,
+            password: newUserData.password,
         });
 
-        admin.auth().setCustomUserClaims(response.uid, { role });
+        auth().setCustomUserClaims(uid, { userRole: newUserData.role });
 
-        if (response.uid) {
-            await admin.firestore().collection('users').doc(response.uid).set({
-                name,
-                gender,
-                dob,
-                schoolName,
-                std,
-                div,
-                mobileNo1: mobileNo,
-                mobileNo2: altMobileNo,
-                email: response.email,
-                userId: response.uid,
-            });
+        if (uid) {
+            await firestore()
+                .collection('users')
+                .doc(uid)
+                .set({
+                    /**
+                     * otherNewUserData will save all details except password and confirmPassword
+                     */
+                    ...otherNewUserData,
+                    uid,
+                });
         }
 
-        return { actor: 'auth', code: 201, message: 'User created successully' };
+        return { actor: 'auth', code: 201, message: 'User created successfully' };
     } catch (error) {
         throw new HttpsError('already-exists', 'Email ID is already exists. Try another Email ID');
     }

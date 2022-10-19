4d581288120e419c60282c42807b4a27e17329c7
kiranbansode <kiran5120135@gmail.com>
2022-10-19 23:19:18 +0530

    feat(cloudFn): add getSubjectsList cloudFn
    This will return list of all voting subjects from firestore database

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,8 @@ import * as cloudFunctions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 /* -------------- Functions will always be deployed to Mumbai -------------- */
-const clf = cloudFunctions.region('asia-south1');
-export default clf;
+const cloudFn = cloudFunctions.region('asia-south1');
+export default cloudFn;
 /* ----------------------------------- End ---------------------------------- */
 
 admin.initializeApp();
@@ -13,7 +13,9 @@ admin.initializeApp();
 /* -------------------- Import Individual Cloud Functions ------------------- */
 const addNewSubject = require('./addNewSubject');
 const createNewUser = require('./createNewUser');
+const getSubjectsList = require('./getSubjectsList');
 /* ----------------------------------- End ---------------------------------- */
 
 exports.addNewSubject = addNewSubject.addNewSubject;
 exports.createNewUser = createNewUser.createNewUser;
+exports.getSubjectsList = getSubjectsList.getSubjectsList;

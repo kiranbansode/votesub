705467e6b567fdd8ea32a1fe52f139a9a3ee98ce
kiranbansode <kiran5120135@gmail.com>
2022-11-09 15:10:54 +0530

    refactor(SubjectPage): make code more readable
    Previous code was so confusing. So needed to make some changes.
    Also added LoadingScreen component to SubjectPage

--- src/pages/DashboardPage/SubjectPage/index.tsx
+++ src/pages/DashboardPage/SubjectPage/index.tsx
@@ -1,47 +1,94 @@
 import Header from 'components/Header';
 import { useParams } from 'react-router-dom';
-import useAppSelector from 'hooks/useAppSelector';
-import getSubjectDetailsFromList from 'utils/helperFunctions/getSubjectDetailsFromList';
-import { getTotalVotesCLF } from 'config/firebase';
+import { firestore } from 'config/firebase';
+import { onSnapshot, query, collection, where } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import getCandidatesDetails from 'utils/helperFunctions/getCandidatesDetails';
 import VotingCandidate from 'components/VotingCandidate';
 import Separator from 'components/Separator';
+import LoadingScreen from 'components/LoadingScreen';
+import convertUnixEpochToDate from 'utils/helperFunctions/convertUnixEpoch';
+import getSubjectDetails from 'utils/helperFunctions/getSubjectDetails';
 
 import './SubjectPage.styles.scss';
-import convertUnixEpochToDate from 'utils/helperFunctions/convertUnixEpoch';
+
+interface ISubject {
+    id: string;
+    candidates: any[];
+    submittedBy: string;
+    createdOn: number;
+    subjectName: string;
+    userName: string;
+}
+
+interface ICandidate {
+    candidateName: string;
+    id: string;
+    subjectId: string;
+    votes: number;
+}
 
 const SubjectPage = () => {
-    const { id: subjectId } = useParams();
-    const subjectList = useAppSelector(({ subjectsList }) => subjectsList.list);
+    const [subject, setSubject] = useState<ISubject>();
+    const [candidatesDetails, setCandidatesDetails] = useState<ICandidate[]>();
+    const [totalVotes, setTotalVotes] = useState<number>(0);
+    const [showView, setShowView] = useState<boolean>(false);
 
-    const [totalVotes, setTotalVotes] = useState(null);
-    const [candidatesDetails, setCandidateDetails] = useState([]);
-    const { candidates, subjectName, createdOn, submittedBy } = getSubjectDetailsFromList(
-        subjectList,
-        subjectId!,
+    // Get subject's ID from url
+    const { id: subjectId } = useParams();
+    const { day, shortMonth, year, time } = convertUnixEpochToDate(subject?.createdOn!);
+
+    // Get Realtime Votes
+    const candidatesRef = query(
+        collection(firestore, 'candidates'),
+        where('subjectId', '==', `${subjectId}`),
     );
-    const { day, shortMonth, year, time } = convertUnixEpochToDate(createdOn);
+    const unsubscribeToTotalVotes = onSnapshot(candidatesRef, (querySnapshot) => {
+        const cities: any[] = [];
+        querySnapshot.forEach((doc) => {
+            cities.push(doc.data().votes);
+        });
+        setTotalVotes(cities.reduce((a, b) => a + b));
+    });
 
     useEffect(() => {
-        // @ts-ignore
-        getTotalVotesCLF(candidates).then(({ data }) => setTotalVotes(data));
-        getCandidatesDetails(candidates).then((data) => {
-            // @ts-ignore
-            setCandidateDetails(data);
-        });
-    }, []);
+        // Get subject details
+        if (!subject?.id) {
+            getSubjectDetails(subjectId!)
+                // TODO: Look into this. Types are matching
+                // @ts-ignore
+                .then((data) => setSubject(data))
+                .catch((err) => err);
+        }
 
-    console.log(candidatesDetails);
+        // Get Candidates Details
+        if (subject?.id) {
+            getCandidatesDetails(subject.candidates).then((data: any) => {
+                setCandidatesDetails(data);
+                /**
+                 * Using hardcoded `true` as value for showView.
+                 * Because it is very hard to know that how much time useEffect will get invoked ?
+                 */
+                setShowView(true);
+            });
+        }
 
-    return (
+        // Remove Listener to stop realtime vote updates
+        return () => {
+            unsubscribeToTotalVotes();
+        };
+    }, [subject?.id]);
+
+    return !showView ? (
+        <LoadingScreen />
+    ) : (
         <div className="subject-page-container">
             <Header />
             <div className="page-content">
-                <h1 className="title">{subjectName}</h1>
+                <h1 className="title">{subject?.subjectName}</h1>
                 <Separator />
                 <div className="about-container">
-                    <p className="submitter">By : {submittedBy}</p>
+                    <p className="submitter">By : {subject?.submittedBy}</p>
                     <p className="creation-date">
                         Submitted On : {`${day} ${shortMonth} ${year} at ${time}`}
                     </p>
@@ -52,14 +99,12 @@ const SubjectPage = () => {
                 </div>
 
                 <div className="candidates-container">
-                    {candidatesDetails.map((candidate) => (
+                    {candidatesDetails?.map((candidate) => (
                         <VotingCandidate
-                            // @ts-ignore
                             candidateName={candidate.candidateName}
-                            // @ts-ignore
+                            id={candidate.id}
                             key={candidate.id}
                             position={0}
-                            totalVotes={0}
                         />
                     ))}
                 </div>

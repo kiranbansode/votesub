62220a7a01aba66a2e0db9ac5d6f297fc34f009a
kiranbansode <kiran5120135@gmail.com>
2022-11-10 13:27:07 +0530

    feat(helperFunctions): allow access to authenticated user
    Needs more to work to be fully functional

--- src/utils/helperFunctions/getCurrentLoggedUser.ts
+++ src/utils/helperFunctions/getCurrentLoggedUser.ts
@@ -1,12 +1,24 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { store } from 'store';
 import { SAVE_USER_AUTH_DETAILS } from 'store/loginPage/userLoginSlice';
+import { ALLOW_USER_TO_SIGN_IN, HIDE_LOGIN_PAGE } from 'store/ui';
+
+const hideLoginPage = () => {
+    const loginPage = document.querySelector('#login-page')! as HTMLElement;
+
+    document.addEventListener('DOMContentLoaded', () => {
+        loginPage.style.display = 'none';
+    });
+};
 
 const getCurrentLoggedUser = () => {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
-        if (user) {
+        if (user?.uid) {
+            hideLoginPage();
             store.dispatch(SAVE_USER_AUTH_DETAILS(user));
+            store.dispatch(ALLOW_USER_TO_SIGN_IN());
+            store.dispatch(HIDE_LOGIN_PAGE());
         }
     });
 };

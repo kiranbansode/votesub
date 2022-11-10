8337ed82ebf7899240f124b79bd8d922a2dfc14a
kiranbansode <kiran5120135@gmail.com>
2022-11-10 13:32:12 +0530

    feat(DashboardPage): user can now see all subjects fetched from firestore
    A cloud fn will be invoked to get all subjects from firestore

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,50 +1,23 @@
 import { useEffect } from 'react';
 import Header from 'components/Header';
 import Separator from 'components/Separator';
-import { getSubjectsListCLF } from 'config/firebase';
 import useAppDispatch from 'hooks/useAppDispatch';
 import Pagination from 'components/Pagination';
-
 import { HIDE_SIGN_IN_SUCCESS_POP_UP } from 'store/ui';
+import { getSubjectsListFromFirestore } from 'store/dashboard/subjectsListSlice';
+import useAppSelector from 'hooks/useAppSelector';
 
 import './Dashboard.styles.scss';
 
 const DashboardPage = () => {
     const dispatch = useAppDispatch();
-
-    useEffect(() => {
-        console.log('Calling getVoting');
-        getSubjectsListCLF();
-    }, []);
+    const { subjectsList } = useAppSelector((state) => state);
 
     useEffect(() => {
         dispatch(HIDE_SIGN_IN_SUCCESS_POP_UP());
+        // Get all subjects from firestore when component gets first rendered
+        dispatch(getSubjectsListFromFirestore());
     }, []);
-
-    // const dashboardPageView = (
-    //     <div id="dashboard-page">
-    //         <Header />
-
-    //         <p id="remaining-votes">
-    //             Your Remaining Votes : <span>100</span>
-    //         </p>
-    //         <Separator />
-
-    //         <p className="voting-topic"> -x- Most Favorite Subject -x-</p>
-
-    //         <div id="candidate-list">
-    //             <VotingCandidate candidateName="Geography" position={1} totalVotes={1995} />
-
-    //             <VotingCandidate candidateName="History" position={2} totalVotes={1994} />
-
-    //             <VotingCandidate candidateName="Mathematics" position={3} totalVotes={1993} />
-
-    //             <VotingCandidate candidateName="English" position={4} totalVotes={1992} />
-
-    //             <VotingCandidate candidateName="Science" position={5} totalVotes={1991} />
-    //         </div>
-    //     </div>
-    // );
 
     return (
         <div id="dashboard-page">
@@ -58,7 +31,7 @@ const DashboardPage = () => {
             <p className="voting-topic"> -x- Most Favorite Subjects -x-</p>
 
             <div className="subject-list-container">
-                <Pagination />
+                <Pagination data={subjectsList.list} />
             </div>
         </div>
     );

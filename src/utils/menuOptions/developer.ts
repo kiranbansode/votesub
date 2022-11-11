e903b2acd2688aa24ee869261eb0f3c12750b615
kiranbansode <kiran5120135@gmail.com>
2022-11-11 12:07:35 +0530

    refactor: add id as required field in IOptions interface

--- src/utils/menuOptions/developer.ts
+++ src/utils/menuOptions/developer.ts
@@ -1,13 +1,13 @@
 import { IOptions } from '.';
 
 const devOptions: IOptions[] = [
-    { value: 'int', option: 'Intern' },
-    { value: 'jr', option: 'Junior Engineer' },
-    { value: 'md', option: 'Mid-Level Engineer' },
-    { value: 'sr', option: 'Senior Engineer' },
-    { value: 'stf', option: 'Staff Engineer' },
-    { value: 'pr', option: 'Principal Engineer' },
-    { value: 'tf', option: 'Technical Fellow' },
+    { value: 'int', option: 'Intern', id: '26712a66-610f-11ed-9b6a-0242ac120002' },
+    { value: 'jr', option: 'Junior Engineer', id: '26712f20-610f-11ed-9b6a-0242ac120002' },
+    { value: 'md', option: 'Mid-Level Engineer', id: '267130ba-610f-11ed-9b6a-0242ac120002' },
+    { value: 'sr', option: 'Senior Engineer', id: '26713268-610f-11ed-9b6a-0242ac120002' },
+    { value: 'stf', option: 'Staff Engineer', id: '2671340c-610f-11ed-9b6a-0242ac120002' },
+    { value: 'pr', option: 'Principal Engineer', id: '267135ba-610f-11ed-9b6a-0242ac120002' },
+    { value: 'tf', option: 'Technical Fellow', id: '84347dec-610f-11ed-9b6a-0242ac120002' },
 ];
 
 export default devOptions;
--- src/utils/menuOptions/standards.ts
+++ src/utils/menuOptions/standards.ts
@@ -1,16 +1,16 @@
 import { IOptions } from '.';
 
 export const standardOptions: IOptions[] = [
-    { value: '1', option: 'I' },
-    { value: '2', option: 'II' },
-    { value: '3', option: 'III' },
-    { value: '4', option: 'IV' },
-    { value: '5', option: 'V' },
-    { value: '6', option: 'VI' },
-    { value: '7', option: 'VII' },
-    { value: '8', option: 'VIII' },
-    { value: '9', option: 'IX' },
-    { value: '10', option: 'X' },
+    { value: '1', option: 'I', id: 'bac9b98a-610f-11ed-9b6a-0242ac120002' },
+    { value: '2', option: 'II', id: 'bac9bcdc-610f-11ed-9b6a-0242ac120002' },
+    { value: '3', option: 'III', id: 'bac9beda-610f-11ed-9b6a-0242ac120002' },
+    { value: '4', option: 'IV', id: 'bac9c088-610f-11ed-9b6a-0242ac120002' },
+    { value: '5', option: 'V', id: 'bac9c25e-610f-11ed-9b6a-0242ac120002' },
+    { value: '6', option: 'VI', id: 'bac9c93e-610f-11ed-9b6a-0242ac120002' },
+    { value: '7', option: 'VII', id: 'bac9cace-610f-11ed-9b6a-0242ac120002' },
+    { value: '8', option: 'VIII', id: 'bac9cc18-610f-11ed-9b6a-0242ac120002' },
+    { value: '9', option: 'IX', id: 'bac9cd44-610f-11ed-9b6a-0242ac120002' },
+    { value: '10', option: 'X', id: 'bac9ce7a-610f-11ed-9b6a-0242ac120002' },
 ];
 
 export default standardOptions;

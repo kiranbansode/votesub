e1ba73f4158f99b42c409f7b7715451fd18179c8
kiranbansode <kiran5120135@gmail.com>
2022-11-11 12:00:11 +0530

    feat(styled-components): add ColoredRemainingVotes component
    It will apply different text color based on number of remaining votes

---
+++ src/styled/ColoredRemainingVotes/index.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const ColoredRemainingVotes = styled.span<{ remainingVotes: number }>`
+    color: ${({ remainingVotes }) => {
+        const colors: { [x: string]: string } = {
+            75: '#2979ff;',
+            50: '#69f0ae;',
+            25: '#ffb74d;',
+            0: '#d50000',
+        };
+
+        if (remainingVotes > 75) return colors[75];
+        if (remainingVotes > 50) return colors[50];
+        if (remainingVotes > 25) return colors[25];
+        if (remainingVotes > 0) return colors[0];
+        return '#bdbdbd';
+    }};
+`;
+
+export default ColoredRemainingVotes;

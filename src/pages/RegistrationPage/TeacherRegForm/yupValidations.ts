25a831a72a9d994407299af351f7918935dbd2ad
kiranbansode <kiran5120135@gmail.com>
2022-11-24 23:52:38 +0530

    feat(RegistrationPage): add missing yup validation to all registration forms

--- src/pages/RegistrationPage/TeacherRegForm/yupValidations.ts
+++ src/pages/RegistrationPage/TeacherRegForm/yupValidations.ts
@@ -2,28 +2,29 @@ import * as yup from 'yup';
 
 const TeacherRegFormValidations = yup.object({
     name: yup.object({
-        firstname: yup.string().strict().trim().required('First name is required'),
-        middlename: yup.string().strict().trim().required(),
-        lastname: yup.string().strict().trim().required(),
+        firstName: yup.string().strict().trim().required('First Name is required'),
+        middleName: yup.string().strict().trim(),
+        lastName: yup.string().strict().trim().required('Last Name is required'),
     }),
     gender: yup.string().strict().trim().required(),
-    schoolname: yup.string().strict().trim().required(),
-    mobileNo: yup.string().strict().trim().required(),
-    altMobileNo: yup.string().strict().trim(),
-    emailId: yup.string().strict().trim().email().required(),
+    schoolName: yup.string().strict().trim(),
+    countryCode: yup.string().strict().trim().required('We require your country code'),
+    mob1: yup.string().strict().trim().required('We require a valid phone number'),
+    mob2: yup.string().strict().trim(),
+    emailId: yup.string().strict().trim().email().required('Email ID is required'),
     password: yup
         .string()
         .strict()
         .trim()
-        .required('Please Enter your password')
+        .required('Enter your password')
         .matches(
             // eslint-disable-next-line no-useless-escape
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-            'Must Contain atleast 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
+            'Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
         ),
     confirmPassword: yup
         .string()
-        .required()
+        .required('Confirm your password')
         .oneOf([yup.ref('password'), null], 'Password does not match'),
 });
 

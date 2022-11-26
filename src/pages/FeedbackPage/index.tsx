5328881f2cb4d20ee5efb8976b4e81b5b616f421
kiranbansode <kiran5120135@gmail.com>
2022-11-27 00:16:42 +0530

    feat(FeedbackPage): add 503 to FeedbackPage

--- src/pages/FeedbackPage/index.tsx
+++ src/pages/FeedbackPage/index.tsx
@@ -3,13 +3,28 @@ import SliderInputField from 'components/SliderInputField';
 import Button from 'components/Button';
 import { FieldValues, useForm } from 'react-hook-form';
 import './FeedbackPage.styles.scss';
+import PageTitle from 'components/Title';
+import PageNotFound from 'pages/PageNotFound';
 
 interface IFeedbackPageForm {
     ui: number;
+    fast: number;
 }
 
 const feedbackPageFormDefaultValues: IFeedbackPageForm = {
     ui: 0,
+    fast: 0,
+};
+
+const marksForFeedbackPage = {
+    1: [
+        { value: 0, label: '0' },
+        { value: 1, label: '1' },
+        { value: 2, label: '2' },
+        { value: 3, label: '3' },
+        { value: 4, label: '4' },
+        { value: 5, label: '5' },
+    ],
 };
 
 const FeedbackPage = () => {
@@ -18,34 +33,46 @@ const FeedbackPage = () => {
     });
 
     return (
-        <div className="page" id="feedback-page">
-            <Header />
-            <div className="feedback-page__view">
-                <h2 className="feedback-page__title">Feedback Page</h2>
-
-                <form onSubmit={handleSubmit((data) => console.log(data))}>
-                    <SliderInputField
-                        separateLabel
-                        control={control}
-                        fieldName="ui"
-                        inputLabel="User Interface [UI]"
-                        marks={[
-                            { value: 0, label: '0' },
-                            { value: 1, label: '1' },
-                            { value: 2, label: '2' },
-                            { value: 3, label: '3' },
-                            { value: 4, label: '4' },
-                            { value: 5, label: '5' },
-                        ]}
-                        max={5}
-                        min={0}
-                        step={1}
-                    />
-                    <Button type="submit">Submit</Button>
-                </form>
-            </div>
-        </div>
+        <PageNotFound
+            code={503}
+            mssg="The page you are looking for is under development. Please come back again after some time."
+            title="Under Development"
+        />
     );
+
+    // return (
+    //     <div className="page" id="feedback-page">
+    //         <Header />
+    //         <div className="feedback-page__view">
+    //             <PageTitle title="Feedback" />
+
+    //             <form onSubmit={handleSubmit((data) => console.log(data))}>
+    //                 <SliderInputField
+    //                     separateLabel
+    //                     control={control}
+    //                     fieldName="ui"
+    //                     inputLabel="User Interface [UI]"
+    //                     marks={marksForFeedbackPage[1]}
+    //                     max={5}
+    //                     min={0}
+    //                     step={1}
+    //                 />
+
+    //                 <SliderInputField
+    //                     separateLabel
+    //                     control={control}
+    //                     fieldName="fast"
+    //                     inputLabel="How much fast is it [UI]"
+    //                     marks={marksForFeedbackPage[1]}
+    //                     max={5}
+    //                     min={0}
+    //                     step={1}
+    //                 />
+    //                 <Button type="submit">Submit</Button>
+    //             </form>
+    //         </div>
+    //     </div>
+    // );
 };
 
 export default FeedbackPage;

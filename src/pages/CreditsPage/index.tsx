ed5d2ec90726c046ed6a2b27c8881f8549fed888
kiranbansode <kiran5120135@gmail.com>
2022-11-26 20:30:19 +0530

    feat(Pages): add 503 status to AboutMePage, AdminPage, CreditsPage and SettingsPage
    This commit also includes AboutMePage to Pages

--- src/pages/CreditsPage/index.tsx
+++ src/pages/CreditsPage/index.tsx
@@ -1,11 +1,18 @@
-import Header from 'components/Header';
+// import Header from 'components/Header';
+import PageNotFound from 'pages/PageNotFound';
 import './CreditsPage.styles.scss';
 
 const CreditsPage = () => (
-    <div>
-        <Header />
-        <h2>Credit Page</h2>
-    </div>
+    <PageNotFound
+        code={503}
+        mssg="The page you are looking for is under development. Please come back again after some time."
+        title="Under Development"
+    />
+
+    // <div>
+    //     <Header />
+    //     <h2>Credit Page</h2>
+    // </div>
 );
 
 export default CreditsPage;

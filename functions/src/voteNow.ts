666543792e35d82bdc7b02545d5afd2932a15c98
kiranbansode <kiran5120135@gmail.com>
2022-11-26 20:13:11 +0530

    fix(cloudFn): reduceVotes and voteNow will get execute if remaining votes is <= 0

--- functions/src/voteNow.ts
+++ functions/src/voteNow.ts
@@ -7,19 +7,19 @@ exports.voteNow = cloudFn.https.onCall(async (id, context) => {
     const { HttpsError } = await import('firebase-functions/v1/https');
     const candidateRef = firestore().collection('candidates').doc(id);
     const userRef = firestore().collection('users').doc(String(context.auth?.uid));
-    const userData = await (await userRef.get()).data();
+    const userData = (await userRef.get()).data();
 
     if (!context.auth?.uid) {
         throw new HttpsError('unauthenticated', 'Looks like you did not logged in.');
     }
 
-    if (userData?.remainingVotes === 0) {
+    if (userData?.remainingVotes < 0 || userData?.remainingVotes === 0) {
         throw new HttpsError('resource-exhausted', 'You already gave your all votes');
     }
 
     try {
         let res;
-        if (userData?.remainingVotes !== 0) {
+        if (userData?.remainingVotes > 0) {
             res = await candidateRef.update({ votes: firestore.FieldValue.increment(1) });
         }
         return res;

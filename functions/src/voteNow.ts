ff46778679985f4e98b968b396166220de8b56aa
kiranbansode <kiran5120135@gmail.com>
2022-12-09 14:47:39 +0530

    feat(cloudFn): voteNow fn now also reduce user's remaining votes
    This will help to reduce multiple functions invokations

--- functions/src/voteNow.ts
+++ functions/src/voteNow.ts
@@ -1,28 +1,39 @@
-/* eslint-disable import/extensions */
+import cloudFn from '.';
 
-import cloudFn from './index';
-
-exports.voteNow = cloudFn.https.onCall(async (id, context) => {
+exports.voteNow = cloudFn.https.onCall(async (candidateId, context) => {
     const { firestore } = await import('firebase-admin');
     const { HttpsError } = await import('firebase-functions/v1/https');
-    const candidateRef = firestore().collection('candidates').doc(id);
-    const userRef = firestore().collection('users').doc(String(context.auth?.uid));
+
+    const userId = context.auth?.uid;
+    const candidateRef = firestore().collection('candidates').doc(candidateId);
+    const userRef = firestore().collection('users').doc(String(userId));
     const userData = (await userRef.get()).data();
 
     if (!context.auth?.uid) {
-        throw new HttpsError('unauthenticated', 'Looks like you did not logged in.');
+        throw new HttpsError('unauthenticated', 'Looks like you are not logged in.');
     }
 
     if (userData?.remainingVotes < 0 || userData?.remainingVotes === 0) {
-        throw new HttpsError('resource-exhausted', 'You already gave your all votes');
+        throw new HttpsError('resource-exhausted', 'You already gave all of your votes');
     }
 
     try {
-        let res;
+        let candidateVotes;
+        let userRemainingVotes;
+
         if (userData?.remainingVotes > 0) {
-            res = await candidateRef.update({ votes: firestore.FieldValue.increment(1) });
+            /* This will increment candidate votes by 1 */
+            candidateVotes = await candidateRef.update({
+                votes: firestore.FieldValue.increment(1),
+            });
+
+            /* This will decrement user's remaining votes by 1 */
+            userRemainingVotes = await userRef.update({
+                remainingVotes: firestore.FieldValue.increment(-1),
+            });
         }
-        return res;
+
+        return { candidateVotes, userRemainingVotes };
     } catch (error) {
         return error;
     }

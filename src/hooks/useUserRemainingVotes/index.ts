55a5ed47381d27639f69350134c962efb5e14c3a
kiranbansode <kiran5120135@gmail.com>
2022-12-28 16:13:04 +0530

    feat(hooks): add useUserRemainingVotes hook
    This hook will give live remaining votes of user

---
+++ src/hooks/useUserRemainingVotes/index.ts
@@ -0,0 +1,25 @@
+import { doc, onSnapshot } from 'firebase/firestore';
+import { firestore, auth } from 'config/firebase';
+import { useEffect, useState } from 'react';
+
+const useUserRemainingVotes = () => {
+    const [remainingVotes, setRemainingVotes] = useState(0);
+    const userId = auth.currentUser?.uid;
+
+    useEffect(() => {
+        const closeLiveConnection = onSnapshot(
+            doc(firestore, 'users', userId!),
+            (user) => {
+                const userData = user.data();
+                setRemainingVotes(() => userData?.remainingVotes);
+            },
+            () => {},
+        );
+
+        return () => closeLiveConnection();
+    }, []);
+
+    return [remainingVotes];
+};
+
+export default useUserRemainingVotes;

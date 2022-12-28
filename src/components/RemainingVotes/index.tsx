8bfd005857600e20827059616be9050c2b956d7a
kiranbansode <kiran5120135@gmail.com>
2022-12-28 16:15:02 +0530

    refactor(RemainingVotes): add useUserRemainingVotes hook to get user remaining votes from firestore

--- src/components/RemainingVotes/index.tsx
+++ src/components/RemainingVotes/index.tsx
@@ -1,36 +1,12 @@
-import { useState, useEffect } from 'react';
-import { firestore } from 'config/firebase';
-
 import Separator from 'components/Separator';
-import './RemainingVotes.styles.scss';
-import { doc, onSnapshot } from 'firebase/firestore';
 import ColoredRemainingVotes from 'styled/ColoredRemainingVotes';
 import LoadingScreen from 'components/LoadingScreen';
+import useUserRemainingVotes from 'hooks/useUserRemainingVotes';
 
-interface IRemainingVotes {
-    userId: string;
-}
+import './RemainingVotes.styles.scss';
 
-const RemainingVotes = ({ userId }: IRemainingVotes) => {
-    const [remainingVotes, setRemainingVotes] = useState<number>();
-
-    // eslint-disable-next-line arrow-body-style
-    useEffect(() => {
-        const unsubscribe = userId
-            ? onSnapshot(
-                  doc(firestore, 'users', userId),
-                  (user) => {
-                      const userData = user.data();
-                      setRemainingVotes(() => userData?.remainingVotes);
-                  },
-                  () => {},
-              )
-            : () => {};
-
-        return () => {
-            unsubscribe();
-        };
-    });
+const RemainingVotes = () => {
+    const [remainingVotes] = useUserRemainingVotes();
 
     return remainingVotes || remainingVotes === 0 ? (
         <div className="remaining-votes-container">

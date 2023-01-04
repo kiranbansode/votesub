669cef872409985396aeff974bef933fac5ad969
kiranbansode <kiran5120135@gmail.com>
2023-01-04 22:58:27 +0530

    feat(PaginationHeader): add refresh button to get updates subjects list

--- src/components/Pagination/PaginationHeader/index.tsx
+++ src/components/Pagination/PaginationHeader/index.tsx
@@ -1,10 +1,23 @@
-import { MenuItem, Select, FormControl } from '@mui/material';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { MenuItem, Select, FormControl, Tooltip } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-// import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
-import SortIcon from '@mui/icons-material/Sort';
+import RefreshIcon from '@mui/icons-material/Refresh';
+// import SortIcon from '@mui/icons-material/Sort';
 import useAppDispatch from 'hooks/useAppDispatch';
-import { SHOW_ONLY_FIVE, SHOW_ONLY_TEN, SHOW_ONLY_TWENTY } from 'store/dashboard/sortedSubjectList';
+import {
+    RESET_SORTED_SUBJECTS_LIST,
+    SHOW_ONLY_FIVE,
+    SHOW_ONLY_TEN,
+    SHOW_ONLY_TWENTY,
+} from 'store/dashboard/sortedSubjectList';
+import {
+    RESET_UNSORTED_SUBJECTS_LIST,
+    SAVE_UNSORTED_SUBJECTS_LIST,
+} from 'store/dashboard/subjectsListSlice';
 import { RESET_CURRENT_PAGE } from 'store/pagination/pageTracker';
+import getSubjectsFromFirestore from 'utils/helperFunctions/getSubjectsFromFirestore';
+
 import './PaginationHeader.styles.scss';
 
 interface IPaginationHeader {
@@ -56,20 +69,34 @@ const PaginationHeader = ({ unsortedData }: IPaginationHeader) => {
 
             <div className="func-wrapper">
                 <span>
-                    <SearchIcon />
+                    <Tooltip title="Search a Subject">
+                        <SearchIcon />
+                    </Tooltip>
                 </span>
             </div>
 
-            {/* 
-            <div className="func-wrapper">
+            {/* <div className="func-wrapper">
                 <span>
-                    <FilterAltOutlinedIcon />
+                    <Tooltip title="Sort Subjects">
+                        <SortIcon />
+                    </Tooltip>
                 </span>
             </div> */}
 
-            <div className="func-wrapper">
+            <div
+                className="func-wrapper"
+                onClick={() => {
+                    dispatch(RESET_UNSORTED_SUBJECTS_LIST());
+                    dispatch(RESET_SORTED_SUBJECTS_LIST());
+                    getSubjectsFromFirestore().then((data) =>
+                        dispatch(SAVE_UNSORTED_SUBJECTS_LIST(data)),
+                    );
+                }}
+            >
                 <span>
-                    <SortIcon />
+                    <Tooltip title="Refresh Subjects">
+                        <RefreshIcon />
+                    </Tooltip>
                 </span>
             </div>
         </div>

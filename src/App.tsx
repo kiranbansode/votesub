4230bee23ae8f4f36e475795b149873530db83d4
kiranbansode <kiran5120135@gmail.com>
2023-01-05 21:06:21 +0530

    refactor(features): moved getCurrentLoggedUser to features folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { CircularProgress } from '@mui/material';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { lazyWithPreload } from 'react-lazy-with-preload';
 import useAppSelector from 'hooks/useAppSelector';
-import getCurrentLoggedUser from 'utils/helperFunctions/getCurrentLoggedUser';
+import getCurrentLoggedUser from 'features/getCurrentLoggedUser';
 
 import 'animate.css';
 

53897cab377ab9c87da1e58e782bbe019dde942e
Kiran Bansode <kiran5120135@gmail.com>
2023-01-19 01:16:04 +0530

    refactor(TextInputField): component TextInputField will use Controller component instead of register fn

--- src/components/TextInputField/index.tsx
+++ src/components/TextInputField/index.tsx
@@ -4,6 +4,7 @@ import SeparateLabel from 'components/SeparateLabel';
 import InputFieldWrapper from 'styled/InputFieldWrapper';
 import inputErrorMessageFinder from 'utils/helperFunctions/inputErrorMessageFinder';
 import './TextInputField.styles.scss';
+import { Controller } from 'react-hook-form';
 
 /**
  * TextInputField is a wrapper component around Material-UI's TextField.
@@ -19,16 +20,22 @@ function TextInputField({
     noLabel,
     separateLabel,
     errors,
-    formRegister,
     inputHelperText,
     inputLabel,
     inputPlaceholder,
     required,
     makeItTextArea,
+    control,
+    fieldName,
 }: ITextInputFieldProps) {
-    const errorMessage = inputErrorMessageFinder(formRegister.name, errors);
+    // TODO: Find better way to handle empty string
+    if (fieldName === '') {
+        throw Error('fieldName can not be an empty string');
+    }
 
-    let labelToShow;
+    const errorMessage = inputErrorMessageFinder(fieldName, errors);
+
+    let labelToShow: any;
     /*
      * Depending on passed condition it will do one of following
      * Separate Label - Detached from TextInputField
@@ -40,40 +47,34 @@ function TextInputField({
     }
 
     return (
-        <>
-            <InputFieldWrapper>
-                <FormControl error={Boolean(errorMessage)}>
-                    {separateLabel ? (
-                        <>
-                            <SeparateLabel
-                                htmlFor={formRegister.name}
-                                label={inputLabel}
-                                required={required}
-                            />
-                        </>
-                    ) : null}
+        <InputFieldWrapper>
+            <FormControl error={Boolean(errorMessage)}>
+                {separateLabel ? (
+                    <SeparateLabel htmlFor={fieldName} label={inputLabel} required={required} />
+                ) : null}
 
-                    <TextField
-                        fullWidth
-                        autoFocus={autoFocus}
-                        error={Boolean(errorMessage)}
-                        id={formRegister.name}
-                        inputRef={formRegister.ref}
-                        label={labelToShow}
-                        minRows={makeItTextArea ? 3 : 0}
-                        multiline={makeItTextArea}
-                        name={formRegister.name}
-                        placeholder={inputPlaceholder}
-                        onBlur={formRegister.onBlur}
-                        onChange={formRegister.onChange}
-                    />
-
-                    <FormHelperText id={formRegister.name}>
-                        {errorMessage || inputHelperText}
-                    </FormHelperText>
-                </FormControl>
-            </InputFieldWrapper>
-        </>
+                <Controller
+                    control={control}
+                    name={fieldName}
+                    render={({ field }) => (
+                        <TextField
+                            fullWidth
+                            autoFocus={autoFocus}
+                            error={Boolean(errorMessage)}
+                            id={fieldName}
+                            inputRef={field.ref}
+                            label={labelToShow}
+                            minRows={makeItTextArea ? 3 : 0}
+                            multiline={makeItTextArea}
+                            placeholder={inputPlaceholder}
+                            // eslint-disable-next-line react/jsx-props-no-spreading
+                            {...field}
+                        />
+                    )}
+                />
+                <FormHelperText id={fieldName}>{errorMessage || inputHelperText}</FormHelperText>
+            </FormControl>
+        </InputFieldWrapper>
     );
 }
 

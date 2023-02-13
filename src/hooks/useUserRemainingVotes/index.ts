1ba63c31c14ef5585013a787f160ab0757898a72
Kiran Bansode <kiran5120135@gmail.com>
2023-02-13 18:12:18 +0530

    fix(hooks): useUserRemainingVotes hook was not able to set remainingVotes back to 0 due to wrong ref
    Wrong document reference was passed

--- src/hooks/useUserRemainingVotes/index.ts
+++ src/hooks/useUserRemainingVotes/index.ts
@@ -1,10 +1,19 @@
-import { doc, onSnapshot } from 'firebase/firestore';
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { firestore, auth } from 'config/firebase';
 import { useEffect, useState } from 'react';
 
 const useUserRemainingVotes = () => {
-    const [remainingVotes, setRemainingVotes] = useState<number | null>(null);
+    const [remainingVotes, setRemainingVotes] = useState<number>(0);
     const userId = auth.currentUser?.uid;
+    const userRef = doc(firestore, 'users', userId!);
+
+    if (remainingVotes < 0) {
+        // sometimes due to latency user's remaining votes goes below 0
+        // if that happen, this will update user's remaining votes back to 0
+        updateDoc(userRef, {
+            remainingVotes: 0,
+        });
+    }
 
     useEffect(() => {
         const closeLiveConnection = onSnapshot(

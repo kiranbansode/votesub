f3ef182cb4b98f24e8c49566ac2ec21fe3616505
Kiran Bansode <kiran5120135@gmail.com>
2023-02-13 18:10:46 +0530

    feat(ColoredRemainingVotes): add color changing outline to ColoredRemainingVotes component

--- src/styled/ColoredRemainingVotes/index.tsx
+++ src/styled/ColoredRemainingVotes/index.tsx
@@ -1,20 +1,29 @@
 import styled from 'styled-components';
 
+const colors: { [x: string]: string } = {
+    75: '#2979ff;',
+    50: '#00e676;',
+    25: '#ffb74d;',
+    0: '#d50000',
+};
+
 const ColoredRemainingVotes = styled.span<{ remainingVotes: number }>`
     color: ${({ remainingVotes }) => {
-        const colors: { [x: string]: string } = {
-            75: '#2979ff;',
-            50: '#69f0ae;',
-            25: '#ffb74d;',
-            0: '#d50000',
-        };
-
         if (remainingVotes > 75) return colors[75];
         if (remainingVotes > 50) return colors[50];
         if (remainingVotes > 25) return colors[25];
         if (remainingVotes > 0) return colors[0];
         return '#e0e0e0';
     }};
+
+    outline: 1px solid
+        ${({ remainingVotes }) => {
+            if (remainingVotes > 75) return colors[75];
+            if (remainingVotes > 50) return colors[50];
+            if (remainingVotes > 25) return colors[25];
+            if (remainingVotes > 0) return colors[0];
+            return '#787878';
+        }};
 `;
 
 export default ColoredRemainingVotes;

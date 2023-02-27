d3dc0587ca912f33663f8c333988f4d0994245e0
Kiran Bansode <kiran5120135@gmail.com>
2023-02-27 23:09:35 +0530

    feat(cloudFn): every day at 12 am UTC+0, users remaining votes will be refilled using refillVotes cloudFn

--- functions/src/refillVotes.ts
+++ functions/src/refillVotes.ts
@@ -1,3 +1,27 @@
 import cloudFn from '.';
 
-exports.refillVotes = cloudFn.pubsub.topic('refillVotes').onPublish(() => {});
+exports.refillVotes = cloudFn.pubsub
+    // Every day at 12:00 AM
+    .schedule('0 0 * * *')
+    // As per UTC 00:00
+    .timeZone('Etc/UTC')
+    .onRun(async () => {
+        const { firestore } = await import('firebase-admin');
+        const usersRef = firestore().collection('users');
+        const usersId: string[] = [];
+
+        (await usersRef.get()).forEach((userDoc) => {
+            const userId = userDoc.get('uid');
+            usersId.push(userId);
+        });
+
+        if (usersId.length > 0) {
+            Promise.all(
+                usersId.map(async (userId) => {
+                    const userRef = firestore().collection('users').doc(userId);
+                    const res = await userRef.update('remainingVotes', 100);
+                    return res;
+                }),
+            );
+        }
+    });

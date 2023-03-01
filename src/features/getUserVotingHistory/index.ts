6bd64106f6416f8e56ee45154ece772f387558b0
Kiran Bansode <kiran5120135@gmail.com>
2023-03-01 15:29:54 +0530

    feat(features): getUserVotingHistory fn will now also get candidate name and subject name from user's voting history
    If somehow subject or candidate got deleted from firestore, getUserVotingHistory
    fn will get both from user's voting history

--- src/features/getUserVotingHistory/index.ts
+++ src/features/getUserVotingHistory/index.ts
@@ -1,13 +1,51 @@
 import { firestore, auth } from 'config/firebase';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, CollectionReference } from 'firebase/firestore';
 import getSubjectDetails from 'features/getSubjectDetails';
 import getSingleCandidateDetails from 'features/getSingleCandidateDetails';
 
+interface ISingleCandidateHistory {
+    [x: string]: {
+        candidateId: string;
+        candidateName: string;
+        givenVotes: number;
+    };
+}
+
+type TSingleSubjectHistory = ISingleCandidateHistory & {
+    lastUpdateOn: number;
+    subjectName: string;
+};
+
+interface IDayHistoryFromFirestore {
+    createdOn: number;
+    history: {
+        [x: string]: TSingleSubjectHistory;
+    };
+    historyPath: string;
+    id: number;
+    localeDate: {
+        date: number;
+        dateWithZero: number;
+        fullMonth: string;
+        month: number;
+        monthWithZero: string;
+        shortMonth: string;
+        year: number;
+    };
+}
+
 const getUserVotingHistory = async () => {
     const userId = auth.currentUser?.uid!;
-    const historyDataSnap = await getDocs(collection(firestore, 'users', userId, 'votingHistory'));
+    const historyDataSnap = await getDocs(
+        collection(
+            firestore,
+            'users',
+            userId,
+            'votingHistory',
+        ) as CollectionReference<IDayHistoryFromFirestore>,
+    );
 
-    const rawHistoryData: any[] = [];
+    const rawHistoryData: IDayHistoryFromFirestore[] = [];
 
     historyDataSnap.forEach((day) => rawHistoryData.push(day.data()));
 
@@ -18,40 +56,74 @@ const getUserVotingHistory = async () => {
     return Promise.all(
         rawHistoryData
             .map(async (dayHistory) => {
-                const rawSubjects: [subjectId: string, candidates: { [x: string]: number }][] =
+                const rawSubjects: [subjectId: string, candidate: TSingleSubjectHistory][] =
                     Object.entries(dayHistory?.history!);
 
                 const createdOn: number = dayHistory?.createdOn;
                 const localeDate = dayHistory?.localeDate;
                 let lastUpdatedOn: number;
+                let subjectNameFromHistory: string;
+                let candidateNameFromHistory: string;
 
                 return Promise.all(
                     rawSubjects
                         .map(async ([subjectId, candidates]) => {
-                            const rawCandidates: [candidateId: string, givenVotes: number][] =
-                                Object.entries(candidates!);
                             const subjectData: any = await getSubjectDetails(subjectId);
+                            const rawCandidates = Object.entries(candidates!);
+
                             const candidatesData = await Promise.all(
                                 rawCandidates.map(async ([candidateId, givenVotes]) => {
                                     if (candidateId === 'lastUpdatedOn') {
                                         /**
-                                         * Here `candidateId[key]` is going to be `lastUpdatedOn`, not an actual candidateId.
-                                         * `givenVotes` will be `firestore timestamp` at which a particular subject's voting
-                                         * history is changed.
+                                         * Here `candidateId` is going to be `lastUpdatedOn`, not an
+                                         * actual candidateId and `givenVotes` will be `Firestore timestamp`
+                                         * at which a particular subject's voting history is changed.
                                          */
+                                        // @ts-ignore
                                         lastUpdatedOn = givenVotes;
+                                        return null;
+                                    }
+
+                                    if (candidateId === 'subjectName') {
+                                        /**
+                                         * Here `candidateId` is going to be `subjectName`, not an
+                                         * actual candidateId and `givenVotes` will be actual subject name.
+                                         */
+                                        // @ts-ignore
+                                        subjectNameFromHistory = givenVotes;
                                         return null;
                                     }
 
                                     // @ts-ignore
                                     const candidateData = await getSingleCandidateDetails(
+                                        /**
+                                         * Here `candidateId` is going to actual candidate ID which will
+                                         * be present in Firestore.
+                                         */
                                         candidateId,
                                     );
 
+                                    /**
+                                     * Here `givenVotes` will be and object containing `candidateId`,
+                                     * `candidateName` and `givenVotes` properties
+                                     */
+                                    candidateNameFromHistory = givenVotes.candidateName;
+
                                     return {
                                         id: candidateData?.id,
-                                        candidateName: candidateData?.candidateName,
-                                        givenVotes,
+                                        /**
+                                         * If somehow candidate got delete from Firestore, user
+                                         * still can see candidateName instead of Unknown
+                                         */
+                                        candidateName:
+                                            candidateData?.candidateName ||
+                                            candidateNameFromHistory,
+                                        /**
+                                         * Here givenVotes will be a raw singleCandidate object
+                                         * which is going to contain `candidateId`, `candidateName`
+                                         * and `givenVotes` properties
+                                         */
+                                        givenVotes: givenVotes.givenVotes,
                                     };
                                 }),
                             );
@@ -61,15 +133,17 @@ const getUserVotingHistory = async () => {
                                 createdOn,
                                 localeDate,
                                 lastUpdatedOn,
-                                subjectName: subjectData?.subjectName,
+                                subjectName: subjectData?.subjectName || subjectNameFromHistory,
                                 candidates: candidatesData
                                     /**
-                                     * It will filter out lastUpdateOn timestamp from candidates array.
-                                     * If we did not filtered it out, then front end will crash.
+                                     * It will filter out lastUpdateOn timestamp from candidates
+                                     * array. If we did not filtered it out, then front end will
+                                     * crash.
                                      */
                                     .filter((candidate) => candidate !== null)
                                     /**
-                                     * Sort candidates bases on their total given votes from highest to lowest
+                                     * Sort candidates bases on their total given votes from highest
+                                     * to lowest
                                      */
                                     // @ts-ignore
                                     .sort((a, b) => (a.givenVotes > b.givenVotes ? -1 : 1)),
@@ -77,7 +151,8 @@ const getUserVotingHistory = async () => {
                         })
                         .sort((prevSub, currSub) =>
                             /**
-                             * Sort subjects bases on when user last voted on. Last one will be on top
+                             * Sort subjects bases on when user last voted on.
+                             * Last one will be on top
                              */
                             // @ts-ignore
                             prevSub.lastUpdatedOn > currSub.lastUpdatedOn ? 1 : -1,

1364400a2fbee65b7d272407bbb21a11a5c061dd
Kiran Bansode <kiran5120135@gmail.com>
2023-03-14 13:15:55 +0530

    refactor(utils): rename interface IOptions to IOption

--- src/utils/menuOptions/index.ts
+++ src/utils/menuOptions/index.ts
@@ -1,5 +1,11 @@
-export interface IOptions {
+export interface IOption {
+    /**
+     * `value`: actual value of select option
+     */
     value: string | number;
+    /**
+     * 'option': label for option
+     */
     option: string;
     id: string;
 }

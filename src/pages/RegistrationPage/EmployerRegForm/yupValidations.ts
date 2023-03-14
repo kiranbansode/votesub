d606a2bd675c509e7bedf7cebd2f437899f14ce2
Kiran Bansode <kiran5120135@gmail.com>
2023-03-14 12:56:55 +0530

    refactor: replace existing yup validation to utils/yupValidations

--- src/pages/RegistrationPage/EmployerRegForm/yupValidations.ts
+++ src/pages/RegistrationPage/EmployerRegForm/yupValidations.ts
@@ -1,32 +1,20 @@
 import * as yup from 'yup';
+import yupValidations from 'utils/yupValidations';
 
 const EmployerRegFormValidations = yup.object({
     name: yup.object({
-        firstName: yup.string().strict().trim().required('First Name is required'),
-        middleName: yup.string().strict().trim(),
-        lastName: yup.string().strict().trim().required('Last Name is required'),
+        firstName: yupValidations.firstName,
+        middleName: yupValidations.middleName,
+        lastName: yupValidations.lastName,
     }),
-    gender: yup.string().strict().trim().required(),
-    companyName: yup.string().strict().trim(),
-    role: yup.string().strict().trim().required(),
-    countryCode: yup.string().strict().trim().required('We require your country code'),
-    mob1: yup.string().strict().trim().required('We require a valid phone number'),
-    mob2: yup.string().strict().trim(),
-    emailId: yup.string().strict().trim().email().required('Email ID is required'),
-    password: yup
-        .string()
-        .strict()
-        .trim()
-        .required('Enter your password')
-        .matches(
-            // eslint-disable-next-line no-useless-escape
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-            'Must Contain at least 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
-        ),
-    confirmPassword: yup
-        .string()
-        .required('Confirm your password')
-        .oneOf([yup.ref('password'), null], 'Password does not match'),
+    gender: yupValidations.gender,
+    companyName: yupValidations.companyName,
+    role: yupValidations.role,
+    countryCode: yupValidations.countryCode,
+    mob1: yupValidations.mob1,
+    emailId: yupValidations.emailId,
+    password: yupValidations.password,
+    confirmPassword: yupValidations.confirmPassword,
 });
 
 export default EmployerRegFormValidations;
--- src/pages/RegistrationPage/StudentRegForm/yupValidations.ts
+++ src/pages/RegistrationPage/StudentRegForm/yupValidations.ts
@@ -1,33 +1,21 @@
 import * as yup from 'yup';
+import yupValidations from 'utils/yupValidations';
 
 const StudentRegFormValidations = yup.object({
     name: yup.object({
-        firstName: yup.string().strict().trim().required('First Name is required'),
-        middleName: yup.string().strict().trim(),
-        lastName: yup.string().strict().trim().required('Last Name is required'),
+        firstName: yupValidations.firstName,
+        middleName: yupValidations.middleName,
+        lastName: yupValidations.lastName,
     }),
-    gender: yup.string().strict().trim().required(),
-    schoolName: yup.string().strict().trim(),
-    std: yup.string().strict().trim(),
-    div: yup.string().strict().trim(),
-    countryCode: yup.string().strict().trim().required('We require your country code'),
-    mob1: yup.string().strict().trim().required('We require a valid phone number'),
-    mob2: yup.string().strict().trim(),
-    emailId: yup.string().strict().trim().email().required('Email ID is required'),
-    password: yup
-        .string()
-        .strict()
-        .trim()
-        .required('Enter your password')
-        .matches(
-            // eslint-disable-next-line no-useless-escape
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
-            'Must Contain minimum 8 Characters, One Uppercase, One Lowercase, One Number and One Special Case Character',
-        ),
-    confirmPassword: yup
-        .string()
-        .required('Confirm your password')
-        .oneOf([yup.ref('password'), null], 'Password does not match'),
+    gender: yupValidations.gender,
+    schoolName: yupValidations.schoolName,
+    std: yupValidations.std,
+    div: yupValidations.div,
+    countryCode: yupValidations.countryCode,
+    mob1: yupValidations.mob1,
+    emailId: yupValidations.emailId,
+    password: yupValidations.password,
+    confirmPassword: yupValidations.confirmPassword,
 });
 
 export default StudentRegFormValidations;

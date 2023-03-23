ba3dbf925cdbe9392b3af85a3c4f5dd0ebfd6d1a
Kiran Bansode <kiran5120135@gmail.com>
2023-03-23 21:36:19 +0530

    refactor(utils): remove 'Consultancy' options from hr

--- src/utils/menuOptions/hr.ts
+++ src/utils/menuOptions/hr.ts
@@ -7,7 +7,6 @@ const hrOptions: IOption[] = [
     { option: 'HR Manager', value: 'hrm', id: '6b218060-6c23-11ed-a1eb-0242ac120002' },
     { option: 'HR', value: 'hr', id: '6b2185a6-6c23-11ed-a1eb-0242ac120002' },
     { option: 'Recruiter', value: 'hrr', id: '6b2186c8-6c23-11ed-a1eb-0242ac120002' },
-    { option: 'Consultancy Firm', value: 'cf', id: 'a807d8a8-6c23-11ed-a1eb-0242ac120002' },
 ];
 
 export default hrOptions;

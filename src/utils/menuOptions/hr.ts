27de1a0ea5686d979a551d373a554aff1221c9df
Kiran Bansode <kiran5120135@gmail.com>
2023-03-23 14:49:16 +0530

    feat(utils): add Founder and CTO options in hrOption menu

--- src/utils/menuOptions/hr.ts
+++ src/utils/menuOptions/hr.ts
@@ -1,7 +1,9 @@
 import { IOption } from '.';
 
 const hrOptions: IOption[] = [
-    { option: 'CEO', value: 'ceo', id: '6b218420-6c23-11ed-a1eb-0242ac120002' },
+    { option: 'Founder', value: 'founder', id: '93703f7b-7033-4153-9323-e2f096550dad' },
+    { option: 'CEO', value: 'ceo', id: 'ee892673-d262-4d6b-b421-939e1ff7874b' },
+    { option: 'CTO', value: 'cto', id: '6b218420-6c23-11ed-a1eb-0242ac120002' },
     { option: 'HR Manager', value: 'hrm', id: '6b218060-6c23-11ed-a1eb-0242ac120002' },
     { option: 'HR', value: 'hr', id: '6b2185a6-6c23-11ed-a1eb-0242ac120002' },
     { option: 'Recruiter', value: 'hrr', id: '6b2186c8-6c23-11ed-a1eb-0242ac120002' },
